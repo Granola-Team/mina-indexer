5c8d3f9622fc8c9049a7ffbe66ee25e8d2c7752c
Jen Reiss <jenc.reiss24@gmail.com>
2022-12-22 14:37:59 -0600

    update visualizer

--- src/bin/graph-visualizer.rs
+++ src/bin/graph-visualizer.rs
@@ -1,16 +1,19 @@
 use mina_indexer::{block_stream::BlockStream, constants::GRAPHQL_URL, subchain::SubchainIndexer};
+use tokio::{fs::File, io::AsyncWriteExt};
 
-use std::error::Error;
+use std::{error::Error, env};
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
+    let output_file = env::var("GRAPHVIZ_OUT")?;
     let mut block_stream = BlockStream::new(GRAPHQL_URL).await?;
     let mut subchain_indexer = SubchainIndexer::new();
+    let mut dot_file = File::create(&output_file).await?;
     while let Some(message) = block_stream.next().await {
         if let Some(block) = message {
             subchain_indexer.add_block(block);
-            println!("{}", subchain_indexer.as_dot());
+            dot_file.write_all(&subchain_indexer.as_dot().into_bytes()).await?;
         }
     }
     Ok(())
