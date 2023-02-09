730957ab202b9f0f1213ffa76aa923276a805c21
Jen Reiss <jenc.reiss24@gmail.com>
2023-02-09 10:57:48 -0600

    add aarch64 support for linux and darwin

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, flake-compat, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "x86_64-windows" ] (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
