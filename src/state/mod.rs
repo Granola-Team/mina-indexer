75ae865225244efae04c9e94dd18bf1b3454199f
Quantifier <isaacdefrain@gmail.com>
2023-02-20 10:53:36 -0500

    Add State with stubs

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,2 +1,56 @@
 pub mod ledger;
-pub mod paths;
+// pub mod best_tip;
+// pub mod block;
+// pub mod branch;
+// pub mod store;
+// pub mod voting;
+
+// use self::head::Head;
+// use self::block::{Block, BlockHash};
+// use self::branch::{Branches, Leaves};
+// use self::store::Store;
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Head {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Block {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct BlockHash {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Branch {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Branches {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Leaves {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Store {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Status {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct DanglingBranches {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct StateUpdate {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct RefLog {}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct State {
+    pub root: BlockHash,
+    pub head: Head,
+    pub leaves: Leaves,
+    pub branches: Branches,
+    pub store: Store,
+    pub status: Status,
+    pub dangling: DanglingBranches, // HashSet<Branch>
+    pub reflog: RefLog, // Vec<StateUpdate>
+}
