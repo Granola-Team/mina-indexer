e1d05b47cfd13af4446fe044dd1b209b4532f1a6
Jen Reiss <jenc.reiss24@gmail.com>
2023-03-04 17:45:39 -0600

    cargo fmt

--- tests/precomputed_block.rs
+++ tests/precomputed_block.rs
@@ -1,8 +1,6 @@
 use std::path::PathBuf;
 
-use mina_indexer::precomputed_block::{
-    scanner::LogScanner, BlockLog, LogEntryProcessor
-};
+use mina_indexer::precomputed_block::{scanner::LogScanner, BlockLog, LogEntryProcessor};
 use tokio::io::AsyncReadExt;
 
 #[tokio::test]
