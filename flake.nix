88a4fa3c237e4f50a8502b1c26eb762e01fc4b6b
Trevor Bernard <trevor@granola.team>
2023-03-09 18:29:37 -0500

    Fix aarch64-darwin nix build
    Eliminate unnecessary packages and clean up build inputs

--- flake.nix
+++ flake.nix
@@ -33,20 +33,19 @@
           rustc = rust;
         };
 
-        runtimeDependencies = with pkgs; [
-          openssl
-          llvmPackages.libclang
-          llvmPackages.libcxxClang
-          clang
-          zstd
-        ];
+        runtimeDependencies = with pkgs;
+          [
+            openssl
+            zstd
+          ];
 
         buildDependencies = with pkgs;
           [
+            llvmPackages.libclang
+            clang
             pkg-config
           ]
-          ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security]
-          ++ runtimeDependencies;
+          ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
 
         developmentDependencies = with pkgs;
           [
@@ -60,7 +59,7 @@
           ++ buildDependencies;
 
         LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-        BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.lib.getVersion pkgs.clang}/include";
+        BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.stdenv.cc.cc}/lib/clang/${pkgs.lib.getVersion pkgs.stdenv.cc.cc}/include";
 
         cargo-toml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
       in
@@ -82,6 +81,7 @@
                 export LIBCLANG_PATH="${LIBCLANG_PATH}"
                 export BINDGEN_EXTRA_CLANG_ARGS="${BINDGEN_EXTRA_CLANG_ARGS}"
               '';
+              doCheck = false;
             };
 
             default = mina-indexer;
