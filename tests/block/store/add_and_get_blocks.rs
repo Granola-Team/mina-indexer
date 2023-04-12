7cd0d825d05a6bf5b81e5203a9ebf82b30091a5a
Quantifier <isaacdefrain@gmail.com>
2023-04-12 01:42:03 +0000

    (test) Add time to rocksdb test

--- tests/block/store/add_and_get_blocks.rs
+++ tests/block/store/add_and_get_blocks.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, path::PathBuf};
 
 use mina_indexer::block::{parser::BlockParser, store::BlockStore};
+use tokio::time::Instant;
 
 #[tokio::test]
 async fn rocksdb() {
@@ -11,16 +12,28 @@ async fn rocksdb() {
     let mut bp = BlockParser::new(log_dir).unwrap();
     let mut blocks = HashMap::new();
 
+    let mut n = 0;
+    let adding = Instant::now();
     while let Some(block) = bp.next().await.unwrap() {
         let state_hash = block.state_hash.clone();
         db.add_block(&block).unwrap();
         blocks.insert(state_hash.clone(), block);
         println!("Added {:?}", &state_hash);
+        n += 1;
     }
+    let add_time = adding.elapsed();
 
+    let fetching = Instant::now();
     for (state_hash, block) in blocks.iter() {
         assert_eq!(block, blocks.get(state_hash).unwrap());
     }
+
+    println!("\n~~~~~~~~~~~~~~~~~~");
+    println!("~~~ Benchmarks ~~~");
+    println!("~~~~~~~~~~~~~~~~~~");
+    println!("Number of blocks: {n}");
+    println!("To insert all:    {add_time:?}");
+    println!("To fetch all:     {:?}\n", fetching.elapsed());
 
     tokio::fs::remove_dir_all(store_dir).await.unwrap();
 }
