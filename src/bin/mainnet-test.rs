0e5fcc75581060ab51e02c6c1fe284a9db1c0ee9
Quantifier <isaacdefrain@gmail.com>
2023-06-08 21:52:32 +0000

    (block ingestion) Add binary

---
+++ src/bin/mainnet-test.rs
@@ -0,0 +1,124 @@
+use std::path::PathBuf;
+
+use mina_indexer::block::{parser::BlockParser, BlockHash};
+use mina_indexer::state::IndexerState;
+
+use mina_indexer::state::ledger::genesis;
+use tokio::time::{Duration, Instant};
+
+use bytesize::ByteSize;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let log_dir = args[1]
+        .parse::<PathBuf>()
+        .expect("First arg should be block log dir path");
+    let num_blocks = args[2]
+        .parse::<u32>()
+        .expect("Second arg should be number of blocks");
+
+    let mut bp = BlockParser::new(&log_dir).unwrap();
+
+    const DB_PATH: &str = "./block-store-test";
+    let store_dir = &PathBuf::from(DB_PATH);
+
+    const GENESIS_HASH: &str = "3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ";
+    let genesis_path = &PathBuf::from("./tests/data/genesis_ledgers/mainnet.json");
+    let genesis_root = genesis::parse_file(genesis_path).await.unwrap();
+
+    let mut state = IndexerState::new(
+        BlockHash(GENESIS_HASH.to_string()),
+        Some(genesis_root.ledger),
+        Some(&PathBuf::from(store_dir)),
+    )
+    .unwrap();
+
+    let total = Instant::now();
+    let mut adding = Duration::new(0, 0);
+    let mut block_count = 1;
+
+    for _ in 1..num_blocks {
+        match bp.next().await {
+            Err(err) => {
+                println!("{err:?}");
+            }
+            Ok(None) => {
+                println!("Ran out of blocks");
+                break;
+            }
+            Ok(Some(block)) => {
+                let add = Instant::now();
+                state.add_block(&block).unwrap();
+                adding += add.elapsed();
+                block_count += 1;
+            }
+        }
+    }
+
+    let total_add = adding;
+    let total_time = total.elapsed();
+
+    println!("\n~~~~~~~~~~~~~~~~~~");
+    println!("~~~ Benchmarks ~~~");
+    println!("~~~~~~~~~~~~~~~~~~");
+    println!("Blocks:  {block_count}");
+    println!("Total:   {total_time:?}");
+    println!(
+        "Per sec: {:?} blocks",
+        block_count as f64 / total_time.as_secs_f64()
+    );
+
+    println!("\n~~~ Add to state ~~~");
+    println!("Avg:     {:?}", total_add / block_count as u32);
+    println!("Total:   {total_add:?}");
+    println!(
+        "Per sec: {:?} blocks",
+        block_count as f64 / total_add.as_secs_f64()
+    );
+
+    println!("\n~~~ DB stats ~~~");
+    let db_stats = state
+        .block_store
+        .as_ref()
+        .unwrap()
+        .database
+        .property_value(rocksdb::properties::DBSTATS)
+        .unwrap()
+        .unwrap();
+    let est_data_size = state
+        .block_store
+        .as_ref()
+        .unwrap()
+        .database
+        .property_int_value(rocksdb::properties::ESTIMATE_LIVE_DATA_SIZE)
+        .unwrap()
+        .unwrap();
+    let est_num_keys = state
+        .block_store
+        .as_ref()
+        .unwrap()
+        .database
+        .property_int_value(rocksdb::properties::ESTIMATE_NUM_KEYS)
+        .unwrap()
+        .unwrap();
+    let curr_size_mem = state
+        .block_store
+        .as_ref()
+        .unwrap()
+        .database
+        .property_int_value(rocksdb::properties::CUR_SIZE_ALL_MEM_TABLES)
+        .unwrap()
+        .unwrap();
+
+    println!("Estimate live data size:    {est_data_size:?}");
+    println!("Estimate number of keys:    {est_num_keys:?}");
+    println!(
+        "Current size all memtables: {:?}",
+        ByteSize::b(curr_size_mem)
+    );
+    println!("{db_stats}");
+
+    tokio::fs::remove_dir_all(store_dir).await.unwrap();
+}
