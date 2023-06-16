cdd91ebce06f7667cbf3cf5e7ef668b517ab3731
Isaac-DeFrain <isaacdefrain@gmail.com>
2023-06-16 00:11:10 +0000

    (lib) Make mainnet genesis hash a const

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub mod state;
 
 pub const SOCKET_NAME: &str = "@mina-indexer.sock";
 pub const MAINNET_TRANSITION_FRONTIER_K: u32 = 290;
+pub const MAINNET_GENESIS_HASH: &str = "3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ";
