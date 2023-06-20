ce7bdb6039c5ac726e5b82c19ac763f5b3636f22
Isaac-DeFrain <quantifier-tech@protonmail.com>
2023-06-20 23:27:01 +0000

    (store) Add CanonicityStore trait

--- src/block/store.rs
+++ src/block/store.rs
@@ -1,6 +1,15 @@
-use super::{precomputed::PrecomputedBlock, BlockHash};
+use crate::{
+    block::{precomputed::PrecomputedBlock, BlockHash},
+    state::Canonicity,
+};
 
 pub trait BlockStore {
     fn add_block(&self, block: &PrecomputedBlock) -> anyhow::Result<()>;
     fn get_block(&self, state_hash: &BlockHash) -> anyhow::Result<Option<PrecomputedBlock>>;
+}
+
+pub trait CanonicityStore {
+    fn get_canonicity(&self, state_hash: &BlockHash) -> anyhow::Result<Option<Canonicity>>;
+    fn add_canonical(&self, state_hash: &BlockHash) -> anyhow::Result<()>;
+    fn add_orphaned(&self, state_hash: &BlockHash) -> anyhow::Result<()>;
 }
