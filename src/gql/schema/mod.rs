fa5b1c79c5f134bfecfd9aefce8117d735144c8f
jstavh <jstavh@gmail.com>
2023-07-26 09:50:26 -0400

    formatting

--- src/gql/schema/mod.rs
+++ src/gql/schema/mod.rs
@@ -1,5 +1,5 @@
-pub use crate::gql::schema::stakes::StakesQueryInput;
 pub use crate::gql::schema::stakes::Stakes;
+pub use crate::gql::schema::stakes::StakesQueryInput;
 pub use crate::gql::schema::transaction::Transaction;
 pub use crate::gql::schema::transaction::TransactionQueryInput;
 
