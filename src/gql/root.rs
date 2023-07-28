8316925f869cd450c324f04febcfd2c5f2dfa41a
jstavh <jstavh@gmail.com>
2023-07-28 13:23:10 -0400

    more formatting after resolving merge conflicts

--- src/gql/root.rs
+++ src/gql/root.rs
@@ -2,9 +2,8 @@ use std::sync::Arc;
 
 use juniper::EmptyMutation;
 use juniper::EmptySubscription;
+use juniper::FieldResult;
 use juniper::RootNode;
-use mina_serialization_types::staged_ledger_diff::UserCommandWithStatusJson;
-use mina_serialization_types::v1::UserCommandWithStatusV1;
 
 use crate::gql::schema::transaction;
 use crate::gql::schema::Stakes;
@@ -12,10 +11,6 @@ use crate::gql::schema::Transaction;
 use crate::gql::schema::TransactionQueryInput;
 use crate::staking_ledger::staking_ledger_store::StakingLedgerStore;
 use crate::store::IndexerStore;
-use juniper::EmptyMutation;
-use juniper::EmptySubscription;
-use juniper::FieldResult;
-use juniper::RootNode;
 
 pub struct Context {
     pub db: Arc<IndexerStore>,
