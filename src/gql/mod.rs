4a02ae3c6c2f5574da871f6f42e569ec4e7dcac1
jstavh <jstavh@gmail.com>
2023-08-08 17:55:31 -0400

    attempting integrating delegations into the stakes query as both a nested and isolated query - WIP

--- src/gql/mod.rs
+++ src/gql/mod.rs
@@ -14,11 +14,15 @@ use actix_web_lab::respond::Html;
 use juniper::http::graphiql::graphiql_source;
 use juniper::http::GraphQLRequest;
 
+use self::schema::delegations::TotalDelegated;
+use crate::delegation_totals_store::create_delegation_totals_db;
+use crate::delegation_totals_store::get_delegation_totals_from_db;
+use crate::delegation_totals_store::update_delegation_totals;
 use crate::gql::root::Context;
 use crate::store::IndexerStore;
 
 mod root;
-mod schema;
+pub(crate) mod schema;
 
 #[get("/graphql")]
 #[allow(clippy::unused_async)]
@@ -26,22 +30,36 @@ async fn graphql_playground() -> impl Responder {
     Html(graphiql_source("/gql", None))
 }
 
-/// GraphQL endpoint
+// GraphQL endpoint
 #[route("/gql", method = "GET", method = "POST")]
 pub async fn gql(
     db: Data<Arc<IndexerStore>>,
     schema: Data<root::Schema>,
     data: Json<GraphQLRequest>,
 ) -> impl Responder {
-    let ctx = Context::new(db.as_ref().clone());
+    let ctx = Context::new(
+        db.as_ref().clone(),
+        create_delegation_totals_db.as_ref().clone(),
+    );
     let res = data.execute(&schema, &ctx).await;
     HttpResponse::Ok().json(res)
 }
 
 pub async fn start_gql(db: Arc<IndexerStore>) -> std::io::Result<()> {
+    //placeholder code for path to delegation_totals_db
+    let delegation_totals_db = Arc::new(
+        create_delegation_totals_db("/path/to/delegation_totals_db")
+            .expect("Failed to create delegation totals DB"),
+    );
+
+    let ctx = Context::new(db.clone(), delegation_totals_db.clone());
+
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(db.clone()))
+            .app_data(Data::new(Context::new(
+                db.clone(),
+                delegation_totals_db.clone(),
+            )))
             .app_data(Data::new(root::create_schema()))
             .service(gql)
             .service(graphql_playground)
--- src/gql/schema/stakes.rs
+++ src/gql/schema/stakes.rs
@@ -2,10 +2,15 @@ use juniper::GraphQLInputObject;
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    gql::root::Context,
+    delegation_totals_store::{self, get_delegation_totals_from_db, update_delegation_totals},
+    gql::{
+        root::Context,
+        schema::delegations::{DelegationTotals, TotalDelegated},
+    },
     staking_ledger::{
         staking_ledger_store::StakingLedgerStore, StakingLedger, StakingLedgerAccount,
     },
+    state::ledger::public_key,
 };
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
@@ -80,9 +85,39 @@ impl StakingLedgerAccount {
     fn delegate(&self) -> &str {
         &self.delegate
     }
-    #[graphql(description = "Account balance")]
+    #[graphql(description = "Account Balance")]
     fn balance(&self) -> f64 {
         self.balance.parse::<f64>().unwrap()
+    }
+    #[graphql(description = "Delegation Totals")]
+    fn delegation_totals(&self) -> Option<DelegationTotals> {
+        // delegation totals for the default epoch (1) here
+        let epoch_number = 1;
+        let public_key = &self.pk; // Use a public key from genesis as default?
+
+        let mut total_delegated = TotalDelegated(0.0);
+        let mut count_delegates = 0;
+
+        if let Some(staking_ledger) = staking_ledger {
+            for account in staking_ledger.accounts {
+                if let Some(delegation_totals) =
+                    get_delegation_totals_from_db(&delegation_totals_db, &account.pk, epoch_number)
+                        .expect("Failed to fetch delegation totals")
+                {
+                    total_delegated.0 += delegation_totals.total_delegated.0;
+                    count_delegates += delegation_totals.count_delegates;
+                }
+            }
+        }
+
+        update_delegation_totals(
+            &delegation_totals_db,
+            "public_key_here", // placeholder code for public key
+            epoch_number,
+            total_delegated,
+            count_delegates,
+        )
+        .expect("Failed to update delegation totals");
     }
 }
 
