daba4ef8a350ad089d9e4de53a059fe6199b8412
Jen Reiss <jenc.reiss24@gmail.com>
2023-10-12 09:08:37 -0500

    fix test

--- flake.nix
+++ flake.nix
@@ -63,10 +63,7 @@
             cargo-audit
             bacon
             google-cloud-sdk
-<<<<<<< HEAD
-=======
             just
->>>>>>> 17f973ea8e655e32227c3dccaf347d70d5d6d064
           ]
           ++ buildDependencies;
 
