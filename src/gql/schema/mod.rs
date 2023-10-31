e3547adca986db2b49e3470701446061d94ac8e8
Jen Reiss <jenc.reiss24@gmail.com>
2023-10-31 11:24:26 -0500

    remove graphql

--- src/gql/schema/mod.rs
+++
@@ -1,7 +0,0 @@
-pub use crate::gql::schema::stakes::Stakes;
-pub use crate::gql::schema::stakes::StakesQueryInput;
-pub use crate::gql::schema::transaction::Transaction;
-pub use crate::gql::schema::transaction::TransactionQueryInput;
-
-pub mod stakes;
-pub mod transaction;
