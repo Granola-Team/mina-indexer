8b1596c0fa76e0a4a072b09493fa685693e5961d
Trevor Bernard <trevor@granola.team>
2023-11-02 12:52:45 -0400

    Remove rocksdb tx and staking-ledgers column families

--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,5 @@
 use crate::{
-    block::{precomputed::PrecomputedBlock, signed_command, store::BlockStore, BlockHash},
+    block::{precomputed::PrecomputedBlock, store::BlockStore, BlockHash},
     staking_ledger::{staking_ledger_store::StakingLedgerStore, StakingLedger},
     state::{
         ledger::{store::LedgerStore, Ledger},
@@ -7,84 +7,16 @@ use crate::{
         Canonicity,
     },
 };
-use data_encoding::BASE32HEX;
-use mina_serialization_types::{staged_ledger_diff::UserCommand, v1::UserCommandWithStatusV1};
 use rocksdb::{
     backup::{BackupEngine, BackupEngineOptions, RestoreOptions},
-    ColumnFamilyDescriptor, DBIterator, DBRawIterator, DB,
+    ColumnFamilyDescriptor, DB,
 };
 use std::{
     fs::remove_dir_all,
     path::{Path, PathBuf},
-    str::FromStr,
 };
 use tracing::{info, instrument, trace};
 use zstd::DEFAULT_COMPRESSION_LEVEL;
-
-/// {Timestamp}-{Height}-{Hash} -> Transaction
-/// The height is padded to 12 digits for sequential iteration
-#[derive(Debug, Clone)]
-pub struct TransactionKey(u64, u32, String);
-
-impl std::fmt::Display for TransactionKey {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // base32hex encode the timestamp so we can maintain datatime sorted order in the keys
-        let bytes = self.0.to_string().into_bytes();
-        let timestamp_hash = BASE32HEX.encode(&bytes);
-
-        write!(f, "{}-{:012}-{}", timestamp_hash, self.1, self.2)
-    }
-}
-
-impl TransactionKey {
-    /// Creates a new key for a transaction
-    pub fn new<S>(h: u32, t: u64, s: S) -> Self
-    where
-        S: Into<String>,
-    {
-        Self(t, h, s.into())
-    }
-
-    /// Returns the key as bytes
-    pub fn bytes(&self) -> Vec<u8> {
-        self.to_string().into_bytes()
-    }
-
-    /// Creates a new key from a slice
-    pub fn from_slice(bytes: &[u8]) -> anyhow::Result<Self> {
-        let key = std::str::from_utf8(bytes)?;
-        let parts: Vec<&str> = key.split('-').collect();
-
-        if parts.len() != 3 {
-            anyhow::bail!("Invalid transaction key: {}", key);
-        }
-        // decode timestamp hash
-        let input = parts[0].as_bytes();
-        let mut output = vec![0; BASE32HEX.decode_len(input.len()).unwrap()];
-        let len = BASE32HEX.decode_mut(input, &mut output).unwrap();
-        let millis_str = std::str::from_utf8(&output[0..len]).unwrap();
-        Ok(Self(
-            u64::from_str(millis_str)?,
-            u32::from_str(parts[1])?,
-            parts[2].to_string(),
-        ))
-    }
-
-    /// Returns the timestamp of the transaction
-    pub fn timestamp(&self) -> u64 {
-        self.0
-    }
-
-    /// Returns the height of the transaction
-    pub fn height(&self) -> u32 {
-        self.1
-    }
-
-    /// Returns the hash of the transaction
-    pub fn hash(&self) -> &str {
-        &self.2
-    }
-}
 
 #[derive(Debug)]
 pub struct IndexerStore {
@@ -111,9 +43,7 @@ impl IndexerStore {
         cf_opts.set_max_write_buffer_number(16);
         let blocks = ColumnFamilyDescriptor::new("blocks", cf_opts.clone());
         let ledgers = ColumnFamilyDescriptor::new("ledgers", cf_opts.clone());
-        let canonicity = ColumnFamilyDescriptor::new("canonicity", cf_opts.clone());
-        let tx = ColumnFamilyDescriptor::new("tx", cf_opts.clone());
-        let staking_ledgers = ColumnFamilyDescriptor::new("staking-ledgers", cf_opts);
+        let canonicity = ColumnFamilyDescriptor::new("canonicity", cf_opts);
 
         let mut database_opts = rocksdb::Options::default();
         database_opts.create_missing_column_families(true);
@@ -121,7 +51,7 @@ impl IndexerStore {
         let database = rocksdb::DBWithThreadMode::open_cf_descriptors(
             &database_opts,
             path,
-            vec![blocks, ledgers, canonicity, tx, staking_ledgers],
+            vec![blocks, ledgers, canonicity],
         )?;
         Ok(Self {
             db_path: PathBuf::from(path),
@@ -131,46 +61,6 @@ impl IndexerStore {
 
     pub fn db_path(&self) -> &Path {
         &self.db_path
-    }
-
-    pub fn put_tx(
-        &self,
-        height: u32,
-        timestamp: u64,
-        tx: UserCommandWithStatusV1,
-    ) -> anyhow::Result<()> {
-        let cf_handle = self.database.cf_handle("tx").expect("column family exists");
-
-        match tx.clone().inner().data.inner().inner() {
-            UserCommand::SignedCommand(cmd) => {
-                let hash = signed_command::SignedCommand(cmd)
-                    .hash_signed_command()
-                    .unwrap();
-                let key = TransactionKey::new(height, timestamp, hash).bytes();
-                let value = bcs::to_bytes(&tx)?;
-
-                self.database.put_cf(&cf_handle, key, value)?;
-
-                Ok(())
-            }
-        }
-    }
-
-    /// Creates a prefix iterator over a CF in the DB
-    pub fn iter_prefix_cf(&self, cf: &str, prefix: &[u8]) -> DBIterator<'_> {
-        let cf_handle = self.database.cf_handle(cf).expect("column family exists");
-        self.database.prefix_iterator_cf(&cf_handle, prefix)
-    }
-
-    pub fn iterator_cf(&self, cf: &str) -> DBIterator<'_> {
-        let cf_handle = self.database.cf_handle(cf).expect("column family exists");
-        self.database
-            .iterator_cf(cf_handle, rocksdb::IteratorMode::Start)
-    }
-
-    pub fn raw_iterator_cf(&self, cf: &str) -> DBRawIterator<'_> {
-        let cf_handle = self.database.cf_handle(cf).expect("column family exists");
-        self.database.raw_iterator_cf(cf_handle)
     }
 
     #[instrument(skip(self))]
