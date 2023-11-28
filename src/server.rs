77d68477d9525bbe41176304556c76c4790f2b05
Trevor Bernard <trevor@granola.team>
2023-11-28 16:24:43 -0500

    Remove MinaIndexerRunPhase (#335)

--- src/server.rs
+++ src/server.rs
@@ -13,7 +13,6 @@ use crate::{
 use anyhow::anyhow;
 use interprocess::local_socket::tokio::LocalSocketListener;
 
-use serde_derive::{Deserialize, Serialize};
 use std::{
     fs,
     path::{Path, PathBuf},
@@ -23,10 +22,7 @@ use std::{
 };
 use tokio::{
     io,
-    sync::{
-        mpsc::{self, Sender},
-        watch,
-    },
+    sync::mpsc::{self, Sender},
     task::JoinHandle,
 };
 use tracing::{debug, info, instrument};
@@ -41,20 +37,6 @@ pub struct IndexerConfiguration {
     pub prune_interval: u32,
     pub canonical_threshold: u32,
     pub canonical_update_threshold: u32,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub enum MinaIndexerRunPhase {
-    JustStarted,
-    ConnectingToIPCSocket,
-    SettingSIGINTHandler,
-    InitializingState,
-    StateInitializedFromParser,
-    StartingBlockReceiver,
-    StartingIPCSocketListener,
-    StartingMainServerLoop,
-    ReceivingBlock,
-    ReceivingIPCConnection,
 }
 
 pub enum MinaIndexerQuery {
@@ -74,7 +56,6 @@ pub enum MinaIndexerQueryResponse {
 pub struct MinaIndexer {
     _ipc_join_handle: JoinHandle<()>,
     _witness_join_handle: JoinHandle<anyhow::Result<()>>,
-    phase_receiver: watch::Receiver<MinaIndexerRunPhase>,
     query_sender:
         mpsc::UnboundedSender<(MinaIndexerQuery, oneshot::Sender<MinaIndexerQueryResponse>)>,
     _ipc_update_sender: Sender<IpcChannelUpdate>,
@@ -93,11 +74,9 @@ impl MinaIndexer {
         config: IndexerConfiguration,
         store: Arc<IndexerStore>,
     ) -> anyhow::Result<Self> {
-        let (phase_sender, phase_receiver) = watch::channel(MinaIndexerRunPhase::JustStarted);
         let (query_sender, query_receiver) = mpsc::unbounded_channel();
         let (ipc_update_sender, ipc_update_receiver) = mpsc::channel::<IpcChannelUpdate>(1);
 
-        let phase_sender = Arc::new(phase_sender);
         let ipc_update_arc = Arc::new(ipc_update_sender.clone());
 
         let watch_dir = config.watch_dir.clone();
@@ -115,25 +94,13 @@ impl MinaIndexer {
         });
 
         let _witness_join_handle = tokio::spawn(async move {
-            use MinaIndexerRunPhase::*;
-            phase_sender.send_replace(StartingIPCSocketListener);
-
-            let (state, phase_sender) =
-                initialize(config, store, phase_sender, ipc_update_arc.clone()).await?;
-            run(
-                watch_dir,
-                state,
-                phase_sender,
-                query_receiver,
-                ipc_update_arc.clone(),
-            )
-            .await
+            let state = initialize(config, store, ipc_update_arc.clone()).await?;
+            run(watch_dir, state, query_receiver, ipc_update_arc.clone()).await
         });
 
         Ok(Self {
             _ipc_join_handle,
             _witness_join_handle,
-            phase_receiver,
             query_sender,
             _ipc_update_sender: ipc_update_sender,
         })
@@ -152,18 +119,6 @@ impl MinaIndexer {
         response_receiver.recv().map_err(|recv_err| recv_err.into())
     }
 
-    pub fn initialized(&self) -> bool {
-        use MinaIndexerRunPhase::*;
-        !matches!(
-            *self.phase_receiver.borrow(),
-            JustStarted | SettingSIGINTHandler | InitializingState
-        )
-    }
-
-    pub fn state(&self) -> MinaIndexerRunPhase {
-        *self.phase_receiver.borrow()
-    }
-
     pub fn blocks_processed(&self) -> anyhow::Result<u32> {
         match self.send_query(MinaIndexerQuery::NumBlocksProcessed)? {
             MinaIndexerQueryResponse::NumBlocksProcessed(blocks_processed) => Ok(blocks_processed),
@@ -175,12 +130,8 @@ impl MinaIndexer {
 pub async fn initialize(
     config: IndexerConfiguration,
     store: Arc<IndexerStore>,
-    phase_sender: Arc<watch::Sender<MinaIndexerRunPhase>>,
     ipc_update_sender: Arc<mpsc::Sender<IpcChannelUpdate>>,
-) -> anyhow::Result<(IndexerState, Arc<watch::Sender<MinaIndexerRunPhase>>)> {
-    use MinaIndexerRunPhase::*;
-
-    phase_sender.send_replace(SettingSIGINTHandler);
+) -> anyhow::Result<IndexerState> {
     debug!("Setting Ctrl-C handler");
     ctrlc::set_handler(move || {
         info!("SIGINT received. Exiting.");
@@ -188,7 +139,6 @@ pub async fn initialize(
     })
     .expect("Error setting Ctrl-C handler");
 
-    phase_sender.send_replace(InitializingState);
     info!("Starting mina-indexer server");
     let IndexerConfiguration {
         ledger,
@@ -247,7 +197,6 @@ pub async fn initialize(
                 .await?;
         }
 
-        phase_sender.send_replace(StateInitializedFromParser);
         ipc_update_sender
             .send(IpcChannelUpdate {
                 best_tip: state.best_tip_block().clone(),
@@ -259,28 +208,22 @@ pub async fn initialize(
         state
     };
 
-    Ok((state, phase_sender))
+    Ok(state)
 }
 
 #[instrument(skip_all)]
 pub async fn run(
     block_watch_dir: impl AsRef<Path>,
     mut state: IndexerState,
-    phase_sender: Arc<watch::Sender<MinaIndexerRunPhase>>,
     mut query_receiver: mpsc::UnboundedReceiver<(
         MinaIndexerQuery,
         oneshot::Sender<MinaIndexerQueryResponse>,
     )>,
     ipc_update_sender: Arc<mpsc::Sender<IpcChannelUpdate>>,
 ) -> Result<(), anyhow::Error> {
-    use MinaIndexerRunPhase::*;
-
-    phase_sender.send_replace(StartingBlockReceiver);
     let mut filesystem_receiver = FilesystemReceiver::new(1024, 64).await?;
     filesystem_receiver.load_directory(block_watch_dir.as_ref())?;
     info!("Block receiver set to watch {:?}", block_watch_dir.as_ref());
-
-    phase_sender.send_replace(StartingMainServerLoop);
 
     loop {
         tokio::select! {
@@ -304,7 +247,6 @@ pub async fn run(
             }
 
             block_fut = filesystem_receiver.recv_block() => {
-                phase_sender.send_replace(ReceivingBlock);
                 if let Some(precomputed_block) = block_fut? {
                     let block = BlockWithoutHeight::from_precomputed(&precomputed_block);
                     debug!("Receiving block {block:?}");
