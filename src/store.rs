8dec8515f2be3114c3249f74054c5e593f9e8e83
Quantifier <quantifier-tech@protonmail.com>
2023-12-28 23:46:49 +0000

    (store) Implement CanonicityStore trait for IndexerStore

--- src/store.rs
+++ src/store.rs
@@ -1,12 +1,17 @@
 use crate::{
     block::{precomputed::PrecomputedBlock, store::BlockStore, BlockHash},
+    canonical::store::CanonicityStore,
     state::{
         ledger::{store::LedgerStore, Ledger},
         Canonicity,
     },
 };
 use rocksdb::{ColumnFamilyDescriptor, DB};
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+use tracing::trace;
 
 #[derive(Debug)]
 pub struct IndexerStore {
@@ -21,17 +26,19 @@ impl IndexerStore {
             &database_opts,
             path,
             secondary,
-            vec!["blocks", "ledgers"],
+            vec!["blocks", "canonicity", "ledgers"],
         )?;
         Ok(Self {
             db_path: PathBuf::from(secondary),
             database,
         })
     }
+
     pub fn new(path: &Path) -> anyhow::Result<Self> {
         let mut cf_opts = rocksdb::Options::default();
         cf_opts.set_max_write_buffer_number(16);
         let blocks = ColumnFamilyDescriptor::new("blocks", cf_opts.clone());
+        let canonicity = ColumnFamilyDescriptor::new("canonicity", cf_opts.clone());
         let ledgers = ColumnFamilyDescriptor::new("ledgers", cf_opts);
 
         let mut database_opts = rocksdb::Options::default();
@@ -40,7 +47,7 @@ impl IndexerStore {
         let database = rocksdb::DBWithThreadMode::open_cf_descriptors(
             &database_opts,
             path,
-            vec![blocks, ledgers],
+            vec![blocks, canonicity, ledgers],
         )?;
         Ok(Self {
             db_path: PathBuf::from(path),
@@ -51,96 +58,229 @@ impl IndexerStore {
     pub fn db_path(&self) -> &Path {
         &self.db_path
     }
+
+    fn blocks_cf(&self) -> &rocksdb::ColumnFamily {
+        self.database
+            .cf_handle("blocks")
+            .expect("blocks column family exists")
+    }
+
+    fn canonicity_cf(&self) -> &rocksdb::ColumnFamily {
+        self.database
+            .cf_handle("canonicity")
+            .expect("canonicity column family exists")
+    }
+
+    fn ledgers_cf(&self) -> &rocksdb::ColumnFamily {
+        self.database
+            .cf_handle("ledgers")
+            .expect("ledgers column family exists")
+    }
 }
 
 impl BlockStore for IndexerStore {
+    /// Add the specified block at its state hash
     fn add_block(&self, block: &PrecomputedBlock) -> anyhow::Result<()> {
-        let cf_handle = self
-            .database
-            .cf_handle("blocks")
-            .expect("column family exists");
+        trace!(
+            "Adding block with height {} and hash {}",
+            block.blockchain_length,
+            block.state_hash
+        );
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
         let key = block.state_hash.as_bytes();
         let value = bcs::to_bytes(&block)?;
-        self.database.put_cf(&cf_handle, key, value)?;
+        let blocks_cf = self.blocks_cf();
+        self.database.put_cf(&blocks_cf, key, value)?;
         Ok(())
     }
 
+    /// Get the block with the specified hash
     fn get_block(&self, state_hash: &BlockHash) -> anyhow::Result<Option<PrecomputedBlock>> {
-        let cf_handle = self
-            .database
-            .cf_handle("blocks")
-            .expect("column family exists");
-        let mut precomputed_block = None;
-        self.database.try_catch_up_with_primary().ok();
+        trace!("Getting block with hash {}", state_hash.0);
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
         let key = state_hash.0.as_bytes();
-        if let Some(bytes) = self
+        let blocks_cf = self.blocks_cf();
+        match self
             .database
-            .get_pinned_cf(&cf_handle, key)?
+            .get_pinned_cf(&blocks_cf, key)?
             .map(|bytes| bytes.to_vec())
         {
-            precomputed_block = Some(bcs::from_bytes(&bytes)?);
+            None => Ok(None),
+            Some(bytes) => Ok(Some(bcs::from_bytes(&bytes)?)),
         }
-        Ok(precomputed_block)
     }
 
-    fn set_canonicity(&self, state_hash: &BlockHash, canonicity: Canonicity) -> anyhow::Result<()> {
-        if let Some(precomputed_block) = self.get_block(state_hash)? {
-            let with_canonicity = PrecomputedBlock {
-                canonicity: Some(canonicity),
-                ..precomputed_block
-            };
-            self.add_block(&with_canonicity)?;
+    /// Set the speicifed block's canonicity
+    fn set_block_canonicity(
+        &self,
+        state_hash: &BlockHash,
+        canonicity: Canonicity,
+    ) -> anyhow::Result<()> {
+        trace!("Setting canonicity of block with hash {}", state_hash.0);
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
+        match self.get_block(state_hash)? {
+            None => Ok(()),
+            Some(precomputed_block) => {
+                let with_canonicity = PrecomputedBlock {
+                    canonicity: Some(canonicity),
+                    ..precomputed_block
+                };
+                self.add_block(&with_canonicity)
+            }
         }
+    }
+
+    /// Get the specified block's canonicity
+    fn get_block_canonicity(&self, state_hash: &BlockHash) -> anyhow::Result<Option<Canonicity>> {
+        trace!("Getting canonicity of block with hash {}", state_hash.0);
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
+        match self.get_block(state_hash)? {
+            Some(PrecomputedBlock {
+                canonicity: Some(block_canonicity),
+                ..
+            }) => Ok(Some(block_canonicity)),
+            _ => Ok(None),
+        }
+    }
+}
+
+impl CanonicityStore for IndexerStore {
+    /// Add a canonical state hash at the specified blockchain_length
+    fn add_canonical_block(&self, height: u32, state_hash: &BlockHash) -> anyhow::Result<()> {
+        trace!(
+            "Adding canonical block at height {height} with hash {}",
+            state_hash.0
+        );
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
+        let key = height.to_be_bytes();
+        let value = bcs::to_bytes(&state_hash)?;
+        let canonicity_cf = self.canonicity_cf();
+        self.database.put_cf(&canonicity_cf, key, value)?;
         Ok(())
     }
 
-    fn get_canonicity(&self, state_hash: &BlockHash) -> anyhow::Result<Option<Canonicity>> {
-        let mut canonicity = None;
-        if let Some(PrecomputedBlock {
-            canonicity: Some(block_canonicity),
-            ..
-        }) = self.get_block(state_hash)?
+    /// Get the state hash of the canonical block with the specified blockchain_length
+    fn get_canonical_hash_at_height(&self, height: u32) -> anyhow::Result<Option<BlockHash>> {
+        trace!("Getting canonical hash at height {height}");
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
+        let key = height.to_be_bytes();
+        let canonicity_cf = self.canonicity_cf();
+        match self
+            .database
+            .get_pinned_cf(&canonicity_cf, key)?
+            .map(|bytes| bytes.to_vec())
         {
-            canonicity = Some(block_canonicity);
+            None => Ok(None),
+            Some(bytes) => Ok(Some(bcs::from_bytes(&bytes)?)),
         }
-        Ok(canonicity)
+    }
+
+    /// Get the length of the canonical chain
+    fn get_max_canonical_blockchain_length(&self) -> anyhow::Result<Option<u32>> {
+        trace!("Getting max canonical blockchain length");
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
+        let canonicity_cf = self.canonicity_cf();
+        match self
+            .database
+            .get_pinned_cf(&canonicity_cf, Self::MAX_CANONICAL_KEY)?
+            .map(|bytes| bytes.to_vec())
+        {
+            None => Ok(None),
+            Some(bytes) => Ok(Some(bcs::from_bytes(&bytes)?)),
+        }
+    }
+
+    /// Set the length of the canonical chain
+    fn set_max_canonical_blockchain_length(&self, height: u32) -> anyhow::Result<()> {
+        trace!("Setting max canonical blockchain length to {height}");
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
+        let canonicity_cf = self.canonicity_cf();
+        let value = bcs::to_bytes(&height)?;
+        self.database
+            .put_cf(&canonicity_cf, Self::MAX_CANONICAL_KEY, value)?;
+        Ok(())
     }
 }
 
 impl LedgerStore for IndexerStore {
+    /// Add the specified ledger at the key `state_hash`
     fn add_ledger(&self, state_hash: &BlockHash, ledger: Ledger) -> anyhow::Result<()> {
-        let cf_handle = self
-            .database
-            .cf_handle("ledgers")
-            .expect("column family exists");
+        trace!("Adding ledger at {}", state_hash.0);
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
         let key = state_hash.0.as_bytes();
-        let value = bcs::to_bytes(&ledger)?;
-        self.database.put_cf(&cf_handle, key, value)?;
+        let value = bcs::to_bytes(&ledger.to_string())?;
+        let ledgers_cf = self.ledgers_cf();
+        self.database.put_cf(&ledgers_cf, key, value)?;
         Ok(())
     }
 
+    /// Get the ledger at the specified state hash
     fn get_ledger(&self, state_hash: &BlockHash) -> anyhow::Result<Option<Ledger>> {
-        let mut ledger = None;
-        let key = state_hash.0.as_bytes();
-        let cf_handle = self
-            .database
-            .cf_handle("ledgers")
-            .expect("column family exists");
+        trace!("Getting ledger at {}", state_hash.0);
+        self.database.try_catch_up_with_primary().unwrap_or(());
 
-        self.database.try_catch_up_with_primary().ok();
+        let ledgers_cf = self.ledgers_cf();
+        let mut state_hash = state_hash.clone();
+        let mut to_apply = vec![];
 
-        if let Some(bytes) = self
+        // walk chain back to a stored ledger (canonical)
+        // collect blocks to compute the current ledger
+        while self
             .database
-            .get_pinned_cf(&cf_handle, key)?
-            .map(|bytes| bytes.to_vec())
+            .get_pinned_cf(&ledgers_cf, state_hash.0.as_bytes())?
+            .is_none()
         {
-            ledger = Some(bcs::from_bytes(&bytes)?);
+            if let Some(block) = self.get_block(&state_hash)? {
+                to_apply.push(block.clone());
+                state_hash = block.previous_state_hash();
+            } else {
+                return Ok(None);
+            }
         }
-        Ok(ledger)
+
+        to_apply.reverse();
+
+        let key = state_hash.0.as_bytes();
+        if let Some(mut ledger) = self
+            .database
+            .get_pinned_cf(&ledgers_cf, key)?
+            .map(|bytes| bytes.to_vec())
+            .map(|bytes| Ledger::from_str(bcs::from_bytes(&bytes).unwrap()).unwrap())
+        {
+            for block in to_apply {
+                ledger.apply_post_balances(&block);
+            }
+
+            return Ok(Some(ledger));
+        }
+
+        Ok(None)
+    }
+
+    /// Get the canonical ledger at the specified height
+    fn get_ledger_at_height(&self, height: u32) -> anyhow::Result<Option<Ledger>> {
+        trace!("Getting ledger at height {height}");
+        self.database.try_catch_up_with_primary().unwrap_or(());
+
+        match self.get_canonical_hash_at_height(height)? {
+            None => Ok(None),
+            Some(state_hash) => self.get_ledger(&state_hash),
+        }
     }
 }
 
 impl IndexerStore {
+    const MAX_CANONICAL_KEY: &[u8] = "max_canonical_blockchain_length".as_bytes();
+
     pub fn db_stats(&self) -> String {
         self.database
             .property_value(rocksdb::properties::DBSTATS)
