9f4cab551f3c74ae351d3ec5ff71afb683056519
Quantifier <quantifier-tech@protonmail.com>
2024-01-16 03:55:38 +0000

    (ledger) Handle coinbase in ledger diff

--- src/ledger/diff/mod.rs
+++ src/ledger/diff/mod.rs
@@ -2,7 +2,6 @@ pub mod account;
 
 use crate::{
     block::precomputed::PrecomputedBlock,
-    command::Command,
     ledger::{coinbase::Coinbase, diff::account::AccountDiff, PublicKey},
 };
 use serde::{Deserialize, Serialize};
@@ -15,30 +14,35 @@ pub struct LedgerDiff {
 
 impl LedgerDiff {
     /// Compute a ledger diff from the given precomputed block
-    pub fn from_precomputed_block(precomputed_block: &PrecomputedBlock) -> Self {
-        let coinbase = Coinbase::from_precomputed_block(precomputed_block);
-        let coinbase_update = coinbase.clone().as_account_diff();
-        let commands = Command::from_precomputed(precomputed_block); // [A]
-        let mut account_diffs_fees: Vec<AccountDiff> =
-            AccountDiff::from_block_fees(coinbase.receiver, precomputed_block); // [A]
-        let mut account_diffs_transactions = commands
-            .iter()
-            .cloned()
+    pub fn from_precomputed(precomputed_block: &PrecomputedBlock) -> Self {
+        let coinbase = Coinbase::from_precomputed(precomputed_block);
+        let mut account_diff_fees: Vec<AccountDiff> =
+            AccountDiff::from_block_fees(coinbase.receiver.clone(), precomputed_block);
+        let mut account_diff_transactions = precomputed_block
+            .commands()
+            .into_iter()
+            .filter(|x| x.is_applied())
+            .map(|x| x.to_command())
             .flat_map(AccountDiff::from_command)
             .collect();
 
         let mut account_diffs = Vec::new();
-        account_diffs.append(&mut account_diffs_fees);
-        account_diffs.append(&mut account_diffs_transactions);
-        account_diffs.push(coinbase_update);
+        account_diffs.append(&mut account_diff_fees);
+        account_diffs.append(&mut account_diff_transactions);
+
+        // apply coinbase last
+        // only apply if "coinbase" =/= [ "Zero" ]
+        let coinbase_update = coinbase.clone().as_account_diff();
+        if coinbase.is_coinbase_applied() {
+            account_diffs.push(coinbase_update);
+        }
 
         LedgerDiff {
-            public_keys_seen: precomputed_block.block_public_keys(),
+            public_keys_seen: precomputed_block.active_block_public_keys(),
             account_diffs,
         }
     }
 
-    // potentially make immutable later on
     pub fn append(&mut self, other: Self) {
         other.public_keys_seen.into_iter().for_each(|account| {
             self.public_keys_seen.push(account);
