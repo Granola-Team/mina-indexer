d29dbc566922a17d252ee4dd5cb0b7bc88ec7b54
Trevor Bernard <trevor@granola.team>
2024-03-15 18:13:40 -0400

    Refactor indexer code into it's own directory called rust

--- Justfile
+++ Justfile
@@ -13,48 +13,48 @@ default:
   @just --list --justfile {{justfile()}}
 
 prereqs:
-  cargo --version
-  cargo nextest --version
-  cargo audit --version
-  cargo clippy --version
-  cargo machete --help 2>&1 >/dev/null
+  cd rust && cargo --version
+  cd rust && cargo nextest --version
+  cd rust && cargo audit --version
+  cd rust && cargo clippy --version
+  cd rust && cargo machete --help 2>&1 >/dev/null
   jq --version
   check-jsonschema --version
 
 build:
-  cargo build --release
+  cd rust && cargo build --release
 
 clean:
-  cargo clean
+  cd rust && cargo clean
   rm -rf result
 
 format:
-  cargo {{nightly_if_required}} fmt --all
+  cd rust && cargo {{nightly_if_required}} fmt --all
 
 test: test-unit
-  ./test
+  cd rust && ./test
 
 test-ci: lint test-unit
-  ./test
+  cd rust && ./test
 
 test-unit: build
-  cargo nextest run --release
+  cd rust && cargo nextest run --release
 
 test-regression: build
-  ./test
+  cd rust && ./test
 
 test-release: build
-  ./test test_release
+  cd rust && ./test test_release
 
 disallow-unused-cargo-deps:
-  cargo machete Cargo.toml
+  cd rust && cargo machete Cargo.toml
 
 audit:
-  cargo audit
+  cd rust && cargo audit
 
 lint: && audit disallow-unused-cargo-deps
-  cargo {{nightly_if_required}} fmt --all --check
-  cargo clippy --all-targets --all-features -- -D warnings
+  cd rust && cargo {{nightly_if_required}} fmt --all --check
+  cd rust && cargo clippy --all-targets --all-features -- -D warnings
 
 images:
   docker build .
--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           inherit system overlays;
         };
 
-        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust/rust-toolchain.toml;
 
         rustPlatform = pkgs.makeRustPlatform {
           cargo = rust;
@@ -72,27 +72,33 @@
           ]
           ++ buildDependencies;
 
-        cargo-toml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        cargo-toml = builtins.fromTOML (builtins.readFile ./rust/Cargo.toml);
       in
         with pkgs; {
           packages = flake-utils.lib.flattenTree rec {
             mina-indexer = rustPlatform.buildRustPackage rec {
               meta = with lib; {
-                description = "The Mina Indexer is a Mina blockchain analytics tool";
+                description = ''
+                  The Mina Indexer is a re-imagined version of the software collectively called the "Mina archive node."
+                '';
                 longDescription = ''
-                  The Mina Indexer is a re-designed version of the software collectively called the "Mina archive node."
+                  The Mina Indexer (indexer) is a re-imagined version of the software collectively called the "Mina archive node." It uses precomputed blocks to reconstitute the historical state of the Mina blockchain. The redesign focuses on improving ease of use and accessibility for developers to interact with the Mina blockchain.
                 '';
                 homepage = "https://github.com/Granola-Team/mina-indexer";
                 license = licenses.asl20;
                 mainProgram = "mina-indexer";
                 platforms = platforms.all;
+                maintainers = [ ];
               };
               pname = cargo-toml.package.name;
               version = cargo-toml.package.version;
 
-              src = ./.;
+              src = ./rust/.;
+              dataDir = ./rust/data;
+              testsDataDir = ./rust/tests/data;
+
               cargoLock = {
-                lockFile = ./Cargo.lock;
+                lockFile = ./rust/Cargo.lock;
               };
 
               nativeBuildInputs = buildDependencies;
@@ -102,6 +108,14 @@
                     (lib.optionalAttrs (stdenv.cc.isClang && stdenv.isDarwin) { NIX_LDFLAGS = "-l${stdenv.cc.libcxx.cxxabi.libName}"; });
 
               doCheck = false;
+              postBuild= ''
+                set -ex
+                echo "Copying over useful files"
+                mkdir -p $out/usr/share/mina-indexer/data
+                cp ${dataDir}/locked.csv $out/usr/share/mina-indexer/data/locked.csv
+                cp -r ${testsDataDir}/genesis_blocks/mainnet-1-3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ.json $out/usr/share/mina-indexer/data
+                cp -r ${testsDataDir}/genesis_ledgers/mainnet.json $out/usr/share/mina-indexer/data
+              '';
             };
             default = mina-indexer;
             dockerImage = pkgs.dockerTools.buildImage {
@@ -118,7 +132,7 @@
                   self
                 ];
                 name = "idx-root";
-                pathsToLink = [ "/bin" ];
+                pathsToLink = [ "/bin" "/usr/share" ];
               };
               config.Cmd = [ "${pkgs.lib.getExe mina-indexer}" ];
             };
