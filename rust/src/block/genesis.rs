fd865b3efd5f1c3907fe927ceda3896f7b7828d2
Trevor Bernard <trevor@granola.team>
2024-03-18 11:35:57 -0400

    Make GenesisBlock work with production docker build

--- rust/src/block/genesis.rs
+++ rust/src/block/genesis.rs
@@ -7,7 +7,15 @@ pub struct GenesisBlock(pub PrecomputedBlock, pub u64);
 impl GenesisBlock {
     /// Creates the mainnet genesis block as a PCB
     pub fn new() -> anyhow::Result<Self> {
-        let genesis_block_path: PathBuf = concat!(env!("PWD"), "/tests/data/genesis_blocks/mainnet-1-3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ.json").into();
+        let genesis_block_path: PathBuf = match std::env::var("RELEASE").unwrap_or_default().as_str() {
+            // TODO: This should be handled up the call stack. Writing here so it builds correctly
+            "production" => {
+                "/usr/share/mina-indexer/data/mainnet-1-3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ.json".into()
+            },
+            _ => {
+                concat!(env!("PWD"), "/tests/data/genesis_blocks/mainnet-1-3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ.json").into()
+            }
+        };
         Ok(Self(
             PrecomputedBlock::parse_file(&genesis_block_path)?,
             genesis_block_path.metadata().unwrap().len(),
