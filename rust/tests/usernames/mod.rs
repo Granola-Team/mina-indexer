7ae50f200cbb792f06f21b4e24e8ca53e7ff5387
Quantifier <quantifier-tech@protonmail.com>
2024-05-30 19:46:17 +0000

    test(name service): Check usernames are set in db

--- rust/tests/usernames/mod.rs
+++ rust/tests/usernames/mod.rs
@@ -1,14 +1,18 @@
 use crate::helpers::setup_new_db_dir;
 use mina_indexer::{
-    block::parser::BlockParser, constants::*, ledger::genesis::GenesisRoot, server::IndexerVersion,
-    state::IndexerState, store::IndexerStore,
+    block::parser::BlockParser,
+    constants::*,
+    ledger::genesis::GenesisRoot,
+    server::IndexerVersion,
+    state::IndexerState,
+    store::{username::UsernameStore, IndexerStore},
 };
 use std::{path::PathBuf, sync::Arc};
 
 #[test]
 fn set_usernames() -> anyhow::Result<()> {
     let store_dir = setup_new_db_dir("usernames-db")?;
-    let blocks_dir = &PathBuf::from("./tests/data/sequential_blocks");
+    let blocks_dir = &PathBuf::from("./tests/data/non_sequential_blocks");
     let store = Arc::new(IndexerStore::new(store_dir.path())?);
     let genesis_contents = include_str!("../data/genesis_ledgers/mainnet.json");
     let genesis_root = serde_json::from_str::<GenesisRoot>(genesis_contents).unwrap();
@@ -27,6 +31,11 @@ fn set_usernames() -> anyhow::Result<()> {
 
     state.add_blocks(&mut bp)?;
 
-    panic!();
-    // Ok(())
+    assert_eq!(
+        "Betelgeuse",
+        store
+            .get_username(&"B62qkEtH1PxqjJPKitAmzfV2ozCuCcibBL4tLgpeXHvsaqVgrENjFhX".into())?
+            .unwrap()
+    );
+    Ok(())
 }
