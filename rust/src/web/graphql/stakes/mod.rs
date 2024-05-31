4565bbd89da73b8b809a106eb4b549771cbe97e9
Quantifier <quantifier-tech@protonmail.com>
2024-05-31 00:16:53 +0000

    fix(gql): Fix stakes query by ledger hash

--- rust/src/web/graphql/stakes/mod.rs
+++ rust/src/web/graphql/stakes/mod.rs
@@ -2,8 +2,10 @@ use super::db;
 use crate::{
     block::store::BlockStore,
     chain::chain_id,
+    command::{internal::store::InternalCommandStore, store::UserCommandStore},
     constants::*,
     ledger::{staking::StakingAccount, store::LedgerStore},
+    snark_work::store::SnarkStore,
     web::graphql::Timing,
 };
 use async_graphql::{ComplexObject, Context, Enum, InputObject, Object, Result, SimpleObject};
@@ -21,7 +23,16 @@ pub struct StakeQueryInput {
 
 #[derive(Enum, Copy, Clone, Eq, PartialEq)]
 pub enum StakeSortByInput {
+    #[graphql(name = "BALANCE_ASC")]
+    BalanceAsc,
+
+    #[graphql(name = "BALANCE_DESC")]
     BalanceDesc,
+
+    #[graphql(name = "STAKE_ASC")]
+    StakeAsc,
+
+    #[graphql(name = "STAKE_DESC")]
     StakeDesc,
 }
 
@@ -47,11 +58,25 @@ impl StakeQueryRoot {
             Some(ref query) => query.epoch.unwrap_or(curr_epoch),
             None => curr_epoch,
         };
-        let staking_ledger = match db.get_staking_ledger_at_epoch(epoch, &None)? {
-            Some(staking_ledger) => staking_ledger,
-            None => return Ok(None),
+
+        // if ledger hash is provided as a query input, use it
+        // else, use the epoch number
+        let staking_ledger = {
+            let opt = if let Some(ledger_hash) = query.as_ref().and_then(|q| q.ledger_hash.clone())
+            {
+                db.get_staking_ledger_hash(&ledger_hash.into())?
+            } else {
+                db.get_staking_ledger_at_epoch(epoch, &None)?
+            };
+            match opt {
+                Some(staking_ledger) => staking_ledger,
+                None => return Ok(None),
+            }
         };
+
         // Delegations will be present if the staking ledger is
+        // (use the staking ledger's epoch)
+        let epoch = staking_ledger.epoch;
         let delegations = db.get_delegations_epoch(epoch, &None)?.unwrap();
 
         let total_currency = staking_ledger.total_currency;
@@ -68,26 +93,35 @@ impl StakeQueryRoot {
                         ledger_hash: query_ledger_hash,
                     } = query;
                     if let Some(public_key) = public_key {
-                        return *public_key == account.pk.0;
+                        if *public_key != account.pk.0 {
+                            return false;
+                        }
                     }
                     if let Some(delegate) = delegate {
-                        return *delegate == account.delegate.0;
+                        if *delegate != account.delegate.0 {
+                            return false;
+                        }
                     }
                     if let Some(query_ledger_hash) = query_ledger_hash {
-                        return *query_ledger_hash == ledger_hash;
+                        if *query_ledger_hash != ledger_hash {
+                            return false;
+                        }
                     }
                     if let Some(query_epoch) = query_epoch {
-                        return *query_epoch == epoch;
-                    }
-                    if let Some(delegate) = delegate {
-                        return *delegate == account.delegate.0;
+                        if *query_epoch != epoch {
+                            return false;
+                        }
                     }
                 }
                 true
             })
             .map(|account| {
                 let pk = account.pk.clone();
-                let result = delegations.delegations.get(&pk).unwrap();
+                let result = delegations
+                    .delegations
+                    .get(&pk)
+                    .cloned()
+                    .unwrap_or_default();
                 let total_delegated_nanomina = result.total_delegated.unwrap_or_default();
                 let count_delegates = result.count_delegates.unwrap_or_default();
                 let mut decimal = Decimal::from(total_delegated_nanomina);
@@ -101,12 +135,32 @@ impl StakeQueryRoot {
                     vesting_increment: Some(timing.vesting_increment),
                     vesting_period: Some(timing.vesting_period),
                 });
+
+                // pk data counts
                 let pk_epoch_num_blocks = db
                     .get_block_production_pk_epoch_count(&pk, Some(epoch))
                     .expect("pk epoch num blocks");
                 let pk_total_num_blocks = db
                     .get_block_production_pk_total_count(&pk)
                     .expect("pk total num blocks");
+                let pk_epoch_num_snarks = db
+                    .get_snarks_pk_epoch_count(&pk, Some(epoch))
+                    .expect("pk epoch num snarks");
+                let pk_total_num_snarks = db
+                    .get_snarks_pk_total_count(&pk)
+                    .expect("pk total num snarks");
+                let pk_epoch_num_user_commands = db
+                    .get_user_commands_pk_epoch_count(&pk, Some(epoch))
+                    .expect("pk epoch num user commands");
+                let pk_total_num_user_commands = db
+                    .get_user_commands_pk_total_count(&pk)
+                    .expect("pk total num user commands");
+                let pk_epoch_num_internal_commands = db
+                    .get_internal_commands_pk_epoch_count(&pk, Some(epoch))
+                    .expect("pk epoch num internal commands");
+                let pk_total_num_internal_commands = db
+                    .get_internal_commands_pk_total_count(&pk)
+                    .expect("pk total num internal commands");
 
                 StakesLedgerAccountWithMeta {
                     epoch,
@@ -115,6 +169,12 @@ impl StakeQueryRoot {
                         account,
                         pk_epoch_num_blocks,
                         pk_total_num_blocks,
+                        pk_epoch_num_snarks,
+                        pk_total_num_snarks,
+                        pk_epoch_num_user_commands,
+                        pk_total_num_user_commands,
+                        pk_epoch_num_internal_commands,
+                        pk_total_num_internal_commands,
                     )),
                     delegation_totals: StakesDelegationTotals {
                         total_currency,
@@ -129,19 +189,47 @@ impl StakeQueryRoot {
                     total_num_blocks: db
                         .get_block_production_total_count()
                         .expect("total block count"),
+                    epoch_num_snarks: db
+                        .get_snarks_epoch_count(Some(epoch))
+                        .expect("epoch snark count"),
+                    total_num_snarks: db.get_snarks_total_count().expect("total snark count"),
+                    epoch_num_user_commands: db
+                        .get_user_commands_epoch_count(Some(epoch))
+                        .expect("epoch user command count"),
+                    total_num_user_commands: db
+                        .get_user_commands_total_count()
+                        .expect("total user command count"),
+                    epoch_num_internal_commands: db
+                        .get_internal_commands_epoch_count(Some(epoch))
+                        .expect("epoch internal command count"),
+                    total_num_internal_commands: db
+                        .get_internal_commands_total_count()
+                        .expect("total internal command count"),
                 }
             })
             .collect();
 
-        if let Some(StakeSortByInput::BalanceDesc) = sort_by {
-            accounts.sort_by(|a, b| b.account.balance_nanomina.cmp(&a.account.balance_nanomina));
-        }
-        if let Some(StakeSortByInput::StakeDesc) = sort_by {
-            accounts.sort_by(|a, b| {
-                b.delegation_totals
-                    .total_delegated_nanomina
-                    .cmp(&a.delegation_totals.total_delegated_nanomina)
-            });
+        match sort_by {
+            Some(StakeSortByInput::BalanceAsc) => {
+                accounts.sort_by(|a, b| a.account.balance_nanomina.cmp(&b.account.balance_nanomina))
+            }
+            Some(StakeSortByInput::BalanceDesc) => {
+                accounts.sort_by(|a, b| b.account.balance_nanomina.cmp(&a.account.balance_nanomina))
+            }
+            Some(StakeSortByInput::StakeAsc) => {
+                accounts.sort_by(|a, b| {
+                    a.delegation_totals
+                        .total_delegated_nanomina
+                        .cmp(&b.delegation_totals.total_delegated_nanomina)
+                });
+            }
+            Some(StakeSortByInput::StakeDesc) | None => {
+                accounts.sort_by(|a, b| {
+                    b.delegation_totals
+                        .total_delegated_nanomina
+                        .cmp(&a.delegation_totals.total_delegated_nanomina)
+                });
+            }
         }
 
         accounts.truncate(limit);
@@ -174,6 +262,30 @@ pub struct StakesLedgerAccountWithMeta {
     /// Value total num blocks
     #[graphql(name = "total_num_blocks")]
     total_num_blocks: u32,
+
+    /// Value epoch num snarks
+    #[graphql(name = "epoch_num_snarks")]
+    epoch_num_snarks: u32,
+
+    /// Value total num snarks
+    #[graphql(name = "total_num_snarks")]
+    total_num_snarks: u32,
+
+    /// Value epoch num user commands
+    #[graphql(name = "epoch_num_user_commands")]
+    epoch_num_user_commands: u32,
+
+    /// Value total num user commands
+    #[graphql(name = "total_num_user_commands")]
+    total_num_user_commands: u32,
+
+    /// Value epoch num internal commands
+    #[graphql(name = "epoch_num_internal_commands")]
+    epoch_num_internal_commands: u32,
+
+    /// Value total num internal commands
+    #[graphql(name = "total_num_internal_commands")]
+    total_num_internal_commands: u32,
 }
 
 #[derive(SimpleObject)]
@@ -221,6 +333,30 @@ pub struct StakesLedgerAccount {
     /// Value pk total num blocks
     #[graphql(name = "pk_total_num_blocks")]
     pub pk_total_num_blocks: u32,
+
+    /// Value pk epoch num snarks
+    #[graphql(name = "pk_epoch_num_snarks")]
+    pk_epoch_num_snarks: u32,
+
+    /// Value pk total num snarks
+    #[graphql(name = "pk_total_num_snarks")]
+    pk_total_num_snarks: u32,
+
+    /// Value pk epoch num user commands
+    #[graphql(name = "pk_epoch_num_user_commands")]
+    pk_epoch_num_user_commands: u32,
+
+    /// Value pk total num user commands
+    #[graphql(name = "pk_total_num_user_commands")]
+    pk_total_num_user_commands: u32,
+
+    /// Value pk epoch num internal commands
+    #[graphql(name = "pk_epoch_num_internal_commands")]
+    pk_epoch_num_internal_commands: u32,
+
+    /// Value pk total num internal commands
+    #[graphql(name = "pk_total_num_internal_commands")]
+    pk_total_num_internal_commands: u32,
 }
 
 #[derive(SimpleObject)]
@@ -255,8 +391,8 @@ impl StakesDelegationTotals {
     }
 }
 
-impl From<(StakingAccount, u32, u32)> for StakesLedgerAccount {
-    fn from(acc: (StakingAccount, u32, u32)) -> Self {
+impl From<(StakingAccount, u32, u32, u32, u32, u32, u32, u32, u32)> for StakesLedgerAccount {
+    fn from(acc: (StakingAccount, u32, u32, u32, u32, u32, u32, u32, u32)) -> Self {
         let balance_nanomina = acc.0.balance;
         let mut decimal = Decimal::from(balance_nanomina);
         decimal.set_scale(9).ok();
@@ -289,6 +425,12 @@ impl From<(StakingAccount, u32, u32)> for StakesLedgerAccount {
             username: None,
             pk_epoch_num_blocks: acc.1,
             pk_total_num_blocks: acc.2,
+            pk_epoch_num_snarks: acc.3,
+            pk_total_num_snarks: acc.4,
+            pk_epoch_num_user_commands: acc.5,
+            pk_total_num_user_commands: acc.6,
+            pk_epoch_num_internal_commands: acc.7,
+            pk_total_num_internal_commands: acc.8,
         }
     }
 }
