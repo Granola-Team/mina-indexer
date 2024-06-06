3dd151ae36f0a844f6542e4963946821aad84051
Quantifier <quantifier-tech@protonmail.com>
2024-06-06 05:33:12 +0000

    feat(rest): Add per account counts

--- rust/src/web/rest/accounts.rs
+++ rust/src/web/rest/accounts.rs
@@ -1,4 +1,10 @@
-use crate::{ledger::store::LedgerStore, store::IndexerStore};
+use crate::{
+    block::store::BlockStore,
+    command::{internal::store::InternalCommandStore, store::UserCommandStore},
+    ledger::{account, public_key::PublicKey, store::LedgerStore},
+    snark_work::store::SnarkStore,
+    store::IndexerStore,
+};
 use actix_web::{
     get,
     http::header::ContentType,
@@ -6,7 +12,22 @@ use actix_web::{
     HttpResponse,
 };
 use log::debug;
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Account {
+    #[serde(flatten)]
+    account: account::Account,
+    epoch_num_blocks: u32,
+    total_num_blocks: u32,
+    epoch_num_snarks: u32,
+    total_num_snarks: u32,
+    epoch_num_user_commands: u32,
+    total_num_user_commands: u32,
+    epoch_num_internal_commands: u32,
+    total_num_internal_commands: u32,
+}
 
 #[get("/accounts/{public_key}")]
 pub async fn get_account(
@@ -16,10 +37,34 @@ pub async fn get_account(
     let db = store.as_ref();
     if let Ok(Some(ledger)) = db.get_best_ledger() {
         debug!("Found best ledger");
-        let account = ledger.accounts.get(&public_key.clone().into());
-        if let Some(ref account) = account {
+        let pk: &PublicKey = &public_key.clone().into();
+        let account = ledger.accounts.get(pk);
+        if let Some(account) = account {
             debug!("Found account in ledger: {:?}", account);
-            let body = serde_json::to_string(account).unwrap();
+            let account = Account {
+                account: account.clone(),
+                epoch_num_blocks: db
+                    .get_block_production_pk_epoch_count(pk, None)
+                    .unwrap_or_default(),
+                total_num_blocks: db
+                    .get_block_production_pk_total_count(pk)
+                    .unwrap_or_default(),
+                epoch_num_snarks: db.get_snarks_pk_epoch_count(pk, None).unwrap_or_default(),
+                total_num_snarks: db.get_snarks_pk_total_count(pk).unwrap_or_default(),
+                epoch_num_user_commands: db
+                    .get_user_commands_pk_epoch_count(pk, None)
+                    .unwrap_or_default(),
+                total_num_user_commands: db
+                    .get_user_commands_pk_total_count(pk)
+                    .unwrap_or_default(),
+                epoch_num_internal_commands: db
+                    .get_internal_commands_pk_epoch_count(pk, None)
+                    .unwrap_or_default(),
+                total_num_internal_commands: db
+                    .get_internal_commands_pk_total_count(pk)
+                    .unwrap_or_default(),
+            };
+            let body = serde_json::to_string_pretty(&account).unwrap();
             return HttpResponse::Ok()
                 .content_type(ContentType::json())
                 .body(body);
