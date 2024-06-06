d1ce3382a1efaa6d95b7bd10404ce1dee888ab67
Robin Bate Boerop <me@robinbb.com>
2024-06-05 18:44:54 -0700

    Remove dup of ops/minaexplorer/download-staking-ledgers

--- scripts/download-ledger.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-destdir="$1"
-epoch="$2"
-ledger_hash="$3"
-
-curl \
-  https://storage.googleapis.com/mina-explorer-ledgers/"$ledger_hash".json \
-  -o "$destdir"/mainnet-"$epoch"-"$ledger_hash".json
