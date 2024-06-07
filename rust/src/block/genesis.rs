6da095451c8fb8ce100b71a782d59564b601f099
Robin Bate Boerop <me@robinbb.com>
2024-06-06 19:26:06 -0700

    Correct command line invocations

--- rust/src/block/genesis.rs
+++ rust/src/block/genesis.rs
@@ -8,10 +8,10 @@ pub struct GenesisBlock(pub PrecomputedBlock, pub u64);
 impl GenesisBlock {
     /// Creates the mainnet genesis block as a PCB
     pub fn new() -> anyhow::Result<Self> {
-        let genesis_block_path =
-            &PathBuf::from("./tests/data/genesis_blocks/mainnet-1-3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ.json");
+        let genesis_block_path: PathBuf =
+            concat!(env!("PWD"), "/tests/data/genesis_blocks/mainnet-1-3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ.json").into();
         Ok(Self(
-            PrecomputedBlock::parse_file(genesis_block_path, PcbVersion::V1)?,
+            PrecomputedBlock::parse_file(&genesis_block_path, PcbVersion::V1)?,
             genesis_block_path.metadata().unwrap().len(),
         ))
     }
