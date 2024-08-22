b66f919d22f9b6de62905749fed709ce18516632
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-08-22 16:15:29 -0300

    rewrite discovery algorithm
    refactor discovery algorithm
    
    refactored algorithm
    
    code optimizations
    
    memory optimizations
    
    clear and extend canonical branch when parent found
    
    remove log statement
    
    refactor parts of the algorithm out into functions
    
    add tests for tricky parts of algorithm
    
    fix lint issues
    
    move best tip algorithm into separate function
    
    rename parent_found to child_found
    
    use btreemap
    
    split the btreemap at the divide between deep and recent canonical
    
    refactorings
    
    augment tests
    
    rename symbol
    
    rename symbol
    
    progress
    
    build canonical branch from latest queued
    
    lint
    
    remove println statement
    
    make funciton signatures more clear about the intent of each variable
    
    add runtime assertion
    
    improve logging
    
    incr database version

--- rust/tests/canonicity/chain_discovery.rs
+++ rust/tests/canonicity/chain_discovery.rs
@@ -4,6 +4,7 @@ use mina_indexer::{
         parser::BlockParser,
         precomputed::{PcbVersion, PrecomputedBlock},
     },
+    canonicity::canonical_chain_discovery::discovery,
     constants::*,
 };
 use std::path::PathBuf;
@@ -124,5 +125,64 @@ async fn canonical_threshold() -> anyhow::Result<()> {
         block_parser.total_num_blocks - canonical_threshold
     );
     assert_eq!(block_parser.total_num_blocks, 20);
+    Ok(())
+}
+
+#[tokio::test]
+async fn discovery_algorithm() -> anyhow::Result<()> {
+    let blocks_dir_str = "tests/data/canonical_chain_discovery/contiguous/";
+    let blocks_dir = PathBuf::from(blocks_dir_str);
+    let pattern = format!("{}/*-*-*.json", blocks_dir.display());
+    let paths: Vec<PathBuf> = glob(&pattern)?.filter_map(|x| x.ok()).collect();
+    let (canonical_paths, recent_paths, orphaned_paths) = discovery(
+        MAINNET_CANONICAL_THRESHOLD,
+        BLOCK_REPORTING_FREQ_NUM,
+        paths.iter().collect(),
+    )?;
+
+    assert_eq!(canonical_paths.len(), 10);
+    assert_eq!(
+        canonical_paths
+            .iter()
+            .map(|p| p.to_str().unwrap())
+            .map(|p| p.strip_prefix(blocks_dir_str).unwrap())
+            .collect::<Vec<_>>(),
+        vec![
+            "mainnet-2-3NLyWnjZqUECniE1q719CoLmes6WDQAod4vrTeLfN7XXJbHv6EHH.json",
+            "mainnet-3-3NKd5So3VNqGZtRZiWsti4yaEe1fX79yz5TbfG6jBZqgMnCQQp3R.json",
+            "mainnet-4-3NL9qBsNibXPm5Nh8cSg5CCqrbzX5VUVY9gJzAbg7EVCF3hfhazG.json",
+            "mainnet-5-3NKQUoBfi9vkbuqtDJmSEYBQrcSo4GjwG8bPCiii4yqM8AxEQvtY.json",
+            "mainnet-6-3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v.json",
+            "mainnet-7-3NLGcwFVQF1p1PrZpusw2fZwBe5HKXGtrGy1Vc4aPkeBtT8nMNUc.json",
+            "mainnet-8-3NLVZQz4FwFbvW4hejfyRpw5NyP8XvQjhj4wSsCjCKdHNBjwWsPG.json",
+            "mainnet-9-3NKknQGpDQu6Afe1VYuHYbEfnjbHT3xGZaFCd8sueL8CoJkx5kPw.json",
+            "mainnet-10-3NKGgTk7en3347KH81yDra876GPAUSoSePrfVKPmwR1KHfMpvJC5.json",
+            "mainnet-11-3NLMeYAFXxsmhSFtLHFxdtjGcfHTVFmBmBF8uTJvP4Ve5yEmxYeA.json",
+        ]
+    );
+
+    assert_eq!(recent_paths.len(), 10);
+    assert_eq!(
+        recent_paths
+            .iter()
+            .map(|p| p.to_str().unwrap())
+            .map(|p| p.strip_prefix(blocks_dir_str).unwrap())
+            .collect::<Vec<_>>(),
+        vec![
+            "mainnet-12-3NKkJDmNZGYdKVDDJkkamGdvNzASia2SXxKpu18imps7KqbNXENY.json",
+            "mainnet-13-3NKXzc1hAE1bK9BSkJUhBBSznMhwW3ZxUTgdoLoqzW6SvqVFcAw5.json",
+            "mainnet-14-3NKDTKbWye6GcdjRu28sSSUgwkNDZXZJvsVZpXAR4YeawhYLqjtE.json",
+            "mainnet-15-3NKkVW47d5Zxi7zvKufBrbiAvLzyKnFgsnN9vgCw65sffvHpv63M.json",
+            "mainnet-16-3NL1sy75LXQScPZda2ywNmdVPiJDnYFe5wV7YLzyRcPVgmDkemW9.json",
+            "mainnet-17-3NKDWsSnHUHN6iakRuBY4LcNou8ToQ3jHpMWkyp6gposjjXC6XUu.json",
+            "mainnet-18-3NLZhhUTMGiWe9UYxY8aYHvRVSoKJTHgKJvopBdC2RA9KisGfPuo.json",
+            "mainnet-19-3NLEu5K5pmEH1CSKZJd94eJatDTM3djoeJTVE3RkcNztJ4z63bM6.json",
+            "mainnet-20-3NLPpt5SyVnD1U5uJAqR3DL1Cqj5dG26SuWutRQ6AQpbQtQUWSYA.json",
+            "mainnet-21-3NKZ6DTHiMtuaeP3tJq2xe4uujVRnGT9FX1rBiZY521uNToSppUZ.json",
+        ]
+    );
+
+    assert_eq!(orphaned_paths.len(), 0);
+
     Ok(())
 }
