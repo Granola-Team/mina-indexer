fd6d65d2743135eea108c4d3eb45f1d491260484
Trevor Bernard <trevor@granola.team>
2024-08-30 06:51:32 -0400

    (devops) include delegate when comparing mainnet state dump and update mina_ledger.json (#1431)
    * update mina_ledger.json export to include the delegate

--- ops/mina-ledger-normalizer.rb
+++ ops/mina-ledger-normalizer.rb
@@ -17,10 +17,11 @@ accounts.each do |account|
   public_key = account['pk']
   nonce = account['nonce'] || '0'
   balance = account['balance']
-
+  delegate = account['delegate'] || public_key
   result[public_key] = {
     'nonce' => nonce,
-    'balance' => balance
+    'balance' => balance,
+    'delegate' => delegate
   }
 end
 
