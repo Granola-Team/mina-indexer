0c73e216549eeeac570157564c9dfa24e96417b8
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-09 14:51:17 -0300

    fix merge

--- rust/src/store/block_store_impl.rs
+++ rust/src/store/block_store_impl.rs
@@ -139,10 +139,10 @@ impl BlockStore for IndexerStore {
 
         // add block internal commands
         self.add_internal_commands_batch(block, &mut batch)?;
-      
+
         // add block internal commands
-        self.add_internal_commands(block)?;
-      
+        self.add_internal_commands_batch(block, &mut batch)?;
+
         info!(
             "Writing {} bytes to database from batch",
             batch.size_in_bytes()
