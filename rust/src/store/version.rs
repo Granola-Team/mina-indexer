9e3ad50bc71f05fa53958687a0b1e022f7ac7599
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-10 12:52:04 -0300

    incr version

--- rust/src/store/version.rs
+++ rust/src/store/version.rs
@@ -25,7 +25,7 @@ pub struct IndexerStoreVersion {
 impl IndexerStoreVersion {
     pub const MAJOR: u32 = 0;
     pub const MINOR: u32 = 10;
-    pub const PATCH: u32 = 47;
+    pub const PATCH: u32 = 48;
 
     /// Output as `MAJOR`.`MINOR`.`PATCH`
     pub fn major_minor_patch(&self) -> String {
