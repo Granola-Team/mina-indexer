3e05e7e82d445afcf4be0d6e4e2bbf83d92eef29
Isaac-DeFrain <isaac@granola.team>
2024-09-17 12:03:41 -0700

    refactor(gql): improve stakes account matching

--- rust/src/web/graphql/stakes/mod.rs
+++ rust/src/web/graphql/stakes/mod.rs
@@ -22,6 +22,7 @@ pub struct StakeQueryInput {
     epoch: Option<u32>,
     delegate: Option<String>,
     ledger_hash: Option<String>,
+
     #[graphql(validator(regex = "^\\d+(\\.\\d{1,9})?$"), name = "stake_lte")]
     stake_lte: Option<String>,
 
@@ -140,18 +141,17 @@ impl StakeQueryRoot {
                 &ledger_hash,
                 epoch,
             ) {
-                accounts.push(StakesLedgerAccountWithMeta::new(
+                let account = StakesLedgerAccountWithMeta::new(
                     db,
                     account,
                     &delegation,
                     epoch,
                     ledger_hash.clone(),
                     total_currency,
-                ));
-                accounts = accounts
-                    .into_iter()
-                    .filter(|x| StakeQueryInput::matches(query.as_ref(), x))
-                    .collect::<Vec<_>>()
+                );
+                if StakeQueryInput::matches(query.as_ref(), &account) {
+                    accounts.push(account);
+                }
             }
         }
         Ok(accounts)
@@ -433,7 +433,7 @@ impl StakeQueryInput {
                 epoch: query_epoch,
                 ledger_hash: query_ledger_hash,
                 username,
-                ..
+                stake_lte: _,
             } = query;
             if let Some(public_key) = public_key {
                 if *public_key != account.pk.0 {
