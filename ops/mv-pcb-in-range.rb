c535c9d542fbeab0e0349e305a2934f8a0768769
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-18 14:17:15 -0300

    add two ruby scripts that I use

---
+++ ops/mv-pcb-in-range.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+# Usage: ./move_and_update_blocks.rb <source_folder> <dest_folder> <blocks_list_file> <min_number> <max_number>
+
+# Check if the correct number of arguments is passed
+if ARGV.length != 5
+  puts "Usage: #{$PROGRAM_NAME} <source_folder> <dest_folder> <blocks_list_file> <min_number> <max_number>"
+  exit 1
+end
+
+# Assign input parameters to variables
+source_folder = ARGV[0]
+dest_folder = ARGV[1]
+blocks_list_file = ARGV[2]
+min_number = ARGV[3].to_i
+max_number = ARGV[4].to_i
+
+# Create the destination folder if it doesn't exist
+Dir.mkdir(dest_folder) unless Dir.exist?(dest_folder)
+
+# Array to track the moved files
+moved_files = []
+
+# Find and move files based on the specified number range
+Dir.foreach(source_folder) do |filename|
+  next unless filename =~ /^mainnet-(\d+)-.*\.json$/
+  number = filename.match(/^mainnet-(\d+)-.*\.json$/)[1].to_i
+
+  # Check if the number is within the range
+  if number >= min_number && number <= max_number
+    source_path = File.join(source_folder, filename)
+    dest_path = File.join(dest_folder, filename)
+
+    # Move the file if it exists
+    if File.exist?(source_path)
+      FileUtils.mv(source_path, dest_path)
+      moved_files << filename
+      puts "Moved: #{filename} to #{dest_folder}/"
+    end
+  end
+end
+
+# Overwrite the blocks.list with the moved files
+File.open(blocks_list_file, 'w') do |file|
+  moved_files.each { |moved_file| file.puts(moved_file) }
+end
+
+puts "Blocks list updated with moved files in #{blocks_list_file}."
---
+++ ops/remove-proofs-from-pcbs.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'find'
+
+# Recursive method to remove all "proofs" properties from a JSON structure
+def remove_proofs(obj)
+  if obj.is_a?(Hash)
+    obj.delete("proofs")
+    obj.each { |key, value| remove_proofs(value) }
+  elsif obj.is_a?(Array)
+    obj.each { |item| remove_proofs(item) }
+  end
+end
+
+# Process all JSON files in a directory
+def process_directory(directory)
+  Find.find(directory) do |path|
+    if path =~ /\.json$/
+      begin
+        file_contents = File.read(path)
+        json_data = JSON.parse(file_contents)
+
+        # Remove all occurrences of "proofs"
+        remove_proofs(json_data)
+
+        # Overwrite the original file with compact JSON (single line)
+        File.open(path, 'w') do |file|
+          file.write(JSON.generate(json_data)) # Compact JSON output
+        end
+
+        puts "Successfully removed 'proofs' from #{path}"
+
+      rescue Errno::ENOENT
+        puts "File not found: #{path}"
+      rescue JSON::ParserError
+        puts "Invalid JSON file: #{path}"
+      end
+    end
+  end
+end
+
+# Usage: ./remove_proofs_from_json_directory_compact.rb <directory>
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <directory>"
+  exit 1
+end
+
+directory = ARGV[0]
+process_directory(directory)
