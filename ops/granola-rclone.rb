f4e60265d294bfb4d85adb32a047df7080e928a0
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-20 09:51:14 -0300

    remove rclone S3 access parameters

--- ops/granola-rclone.rb
+++ ops/granola-rclone.rb
@@ -4,22 +4,11 @@
 
 CONFIG_FILE = "#{__dir__}/rclone.conf"
 
-# It's okay to share these keys publicly, as they are read-only keys for data
-# that we want shared publicly. The data is also available to anyone via HTTPS.
-#
-DEFAULT_ACCESS_KEY = "R58TFO04180PSP69L5TB"
-DEFAULT_SECRET_KEY = "W2OUQ3V2BlhgFQdqHqx6Y0N4mdpDBv3M89trAKml"
-
-access_key = ENV["LINODE_OBJ_ACCESS_KEY"] || DEFAULT_ACCESS_KEY
-secret_key = ENV["LINODE_OBJ_SECRET_KEY"] || DEFAULT_SECRET_KEY
-
 args = [
   "rclone",
   "-vv", # '--log-level', 'INFO',
   "--config", CONFIG_FILE,
   "--buffer-size=128Mi",
-  "--s3-access-key-id=#{access_key}",
-  "--s3-secret-access-key=#{secret_key}",
   *ARGV
 ]
 warn "granola-rclone issuing: #{args}"
