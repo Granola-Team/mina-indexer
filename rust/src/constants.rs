4e6a16b116fd7e8290aca20e44a07503c6d84f48
Quantifier <42081488+Isaac-DeFrain@users.noreply.github.com>
2024-09-25 17:46:00 -0700

    fix(gql): correct bounded queries (#1571)
    * fix(constants): correct millis_since_genesis return type
    
    * feat(txn hash): add from_bytes function
    
    * fix(gql): use various transaction iterators
    
    * chore(store): bump version

--- rust/src/constants.rs
+++ rust/src/constants.rs
@@ -71,8 +71,9 @@ pub fn from_timestamp_millis(millis: i64) -> DateTime<Utc> {
 }
 
 /// Convert epoch milliseconds to global slot number
-pub fn millis_to_global_slot(millis: i64) -> u64 {
-    (millis as u64 - MAINNET_GENESIS_TIMESTAMP) / MAINNET_BLOCK_SLOT_TIME_MILLIS
+pub fn millis_to_global_slot(millis: i64) -> u32 {
+    let millis_since_genesis = millis as u64 - MAINNET_GENESIS_TIMESTAMP;
+    (millis_since_genesis / MAINNET_BLOCK_SLOT_TIME_MILLIS) as u32
 }
 
 pub mod berkeley {
--- rust/src/web/graphql/version/mod.rs
+++ rust/src/web/graphql/version/mod.rs
@@ -1,5 +1,6 @@
-use crate::constants;
-use async_graphql::Object;
+use super::db;
+use crate::{constants, store::version::VersionStore};
+use async_graphql::{Context, Object};
 
 #[derive(Default)]
 pub struct VersionQueryRoot;
@@ -8,5 +9,10 @@ pub struct VersionQueryRoot;
 impl VersionQueryRoot {
     async fn version(&self) -> String {
         constants::VERSION.to_string()
+    }
+
+    async fn db_version(&self, ctx: &Context<'_>) -> anyhow::Result<String> {
+        let db = db(ctx);
+        db.get_db_version().map(|v| v.to_string())
     }
 }
