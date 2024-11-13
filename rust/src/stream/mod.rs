6a2f489df645dc8d491fc68ced864887a51155b4
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-11-13 06:31:21 -0400

    add tests for canonical chain

--- rust/src/stream/mod.rs
+++ rust/src/stream/mod.rs
@@ -5,12 +5,11 @@ use actors::{
     block_summary_persistence_actor::BlockSummaryPersistenceActor, mainnet_block_parser_actor::MainnetBlockParserActor, pcb_path_actor::PCBBlockPathActor,
     snark_work_actor::SnarkWorkSummaryActor, Actor,
 };
-use std::time::Duration;
 use events::Event;
 use futures::future::try_join_all;
 use payloads::GenesisBlockPayload;
 use shared_publisher::SharedPublisher;
-use std::{cmp::Ordering, fs, path::PathBuf, sync::Arc};
+use std::{cmp::Ordering, fs, path::PathBuf, sync::Arc, time::Duration};
 use tokio::{sync::broadcast, task};
 
 mod actors;
@@ -180,13 +179,13 @@ async fn test_process_blocks_dir_with_mainnet_blocks() -> anyhow::Result<()> {
 #[tokio::test]
 async fn test_process_blocks_dir_canonical_updates() -> anyhow::Result<()> {
     use crate::stream::{events::EventType, payloads::BlockCanonicityUpdatePayload};
-    use std::{collections::HashMap, path::PathBuf, str::FromStr};
+    use std::{path::PathBuf, str::FromStr};
     use tokio::{sync::broadcast, time::Duration};
 
     // Create a shutdown channel for the test
     let (shutdown_sender, shutdown_receiver) = broadcast::channel(1);
 
-    // Path to the directory with 100 mainnet block files
+    // Path to the directory with 10 mainnet block files
     let blocks_dir = PathBuf::from_str("./src/stream/test_data/10_mainnet_blocks").expect("Directory with mainnet blocks should exist");
 
     let shared_publisher = Arc::new(SharedPublisher::new(100_000)); // Initialize publisher
@@ -207,21 +206,65 @@ async fn test_process_blocks_dir_canonical_updates() -> anyhow::Result<()> {
     // Wait for the task to finish processing
     let _ = process_handle.await;
 
-    // Count each event type received
-    let mut event_counts: HashMap<EventType, usize> = HashMap::new();
-    let mut last_best_block: Option<BlockCanonicityUpdatePayload> = None;
+    // Define the expected canonicity events based on the detailed log entries provided
+    let expected_canonical_events = vec![
+        // first at height: canonical
+        (1u64, String::from("3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ"), true),
+        // first at height: canonical
+        (2u64, String::from("3NLyWnjZqUECniE1q719CoLmes6WDQAod4vrTeLfN7XXJbHv6EHH"), true),
+        // first at height: canonical
+        (3u64, String::from("3NKd5So3VNqGZtRZiWsti4yaEe1fX79yz5TbfG6jBZqgMnCQQp3R"), true),
+        // first at height: canonical
+        (4u64, String::from("3NL9qBsNibXPm5Nh8cSg5CCqrbzX5VUVY9gJzAbg7EVCF3hfhazG"), true),
+        // first at height: canonical
+        (5u64, String::from("3NKQUoBfi9vkbuqtDJmSEYBQrcSo4GjwG8bPCiii4yqM8AxEQvtY"), true),
+        // first at height: canonical
+        (6u64, String::from("3NKqMEewA8gvEiW7So7nZ3DN6tPnmCtHpWuAzADN5ff9wiqkGf45"), true),
+        // 3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v has greater last_vrf_output
+        (6u64, String::from("3NKqMEewA8gvEiW7So7nZ3DN6tPnmCtHpWuAzADN5ff9wiqkGf45"), false),
+        (6u64, String::from("3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v"), true),
+        // last_vrf_ouput lexicographically smaller than 3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v
+        (6u64, String::from("3NKvdydTvLVDJ9PKAXrisjsXoZQvUy1V2sbComWyB2uyhARCJZ5M"), false),
+        // last_vrf_ouput lexicographically smaller than 3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v
+        (6u64, String::from("3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v"), false),
+        // 3NLM3k3Vk1qs36hZWdbWvi4sqwer3skbgPyHMWrZMBoscNLyjnY2 has greater last_vrf_output
+        (6u64, String::from("3NLM3k3Vk1qs36hZWdbWvi4sqwer3skbgPyHMWrZMBoscNLyjnY2"), true),
+        // block at height 7 links to 3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v at height 6
+        // resulting in branch competition. 3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v at height 6
+        // now becomes canonical, despite having smaller last_vrf_output compared to
+        // 3NLM3k3Vk1qs36hZWdbWvi4sqwer3skbgPyHMWrZMBoscNLyjnY2
+        (6u64, String::from("3NLM3k3Vk1qs36hZWdbWvi4sqwer3skbgPyHMWrZMBoscNLyjnY2"), false),
+        (6u64, String::from("3NKqRR2BZFV7Ad5kxtGKNNL59neXohf4ZEC5EMKrrnijB1jy4R5v"), true),
+        (7u64, String::from("3NL7dd6X6316xu6JtJj6cHwAhHrXwZC4SdBU9TUDUUhfAkB8cSoK"), true),
+        (7u64, String::from("3NL7dd6X6316xu6JtJj6cHwAhHrXwZC4SdBU9TUDUUhfAkB8cSoK"), false),
+        (7u64, String::from("3NLGcwFVQF1p1PrZpusw2fZwBe5HKXGtrGy1Vc4aPkeBtT8nMNUc"), true),
+        (8u64, String::from("3NLVZQz4FwFbvW4hejfyRpw5NyP8XvQjhj4wSsCjCKdHNBjwWsPG"), true),
+        (9u64, String::from("3NKK3QwQbAgMSmrHq4wpgqEwXp5pd9B18CMQjgYsjKTdq8CAsuM6"), true),
+        (9u64, String::from("3NKYjQ6h8xw8RdYvGk8Rc3NnNQHLXjRczUDDZLCXkTJsZFHDhsH6"), false),
+        (9u64, String::from("3NKknQGpDQu6Afe1VYuHYbEfnjbHT3xGZaFCd8sueL8CoJkx5kPw"), false),
+        (9u64, String::from("3NKK3QwQbAgMSmrHq4wpgqEwXp5pd9B18CMQjgYsjKTdq8CAsuM6"), false),
+        (9u64, String::from("3NKknQGpDQu6Afe1VYuHYbEfnjbHT3xGZaFCd8sueL8CoJkx5kPw"), true),
+        (10u64, String::from("3NKGgTk7en3347KH81yDra876GPAUSoSePrfVKPmwR1KHfMpvJC5"), true),
+        (10u64, String::from("3NKGgTk7en3347KH81yDra876GPAUSoSePrfVKPmwR1KHfMpvJC5"), false),
+        (10u64, String::from("3NKHYHrqKpDcon6ToV5CLDiheanjshk5gcsNqefnK78phCFTR2aL"), true),
+    ];
+
+    // Collect actual BlockCanonicityUpdatePayload events received
+    let mut actual_canonical_events = vec![];
     while let Ok(event) = receiver.try_recv() {
-        if event.event_type == EventType::BestBlock {
-            last_best_block = Some(sonic_rs::from_str(&event.payload).unwrap());
+        if event.event_type == EventType::BlockCanonicityUpdate {
+            let payload: BlockCanonicityUpdatePayload = sonic_rs::from_str(&event.payload).unwrap();
+            actual_canonical_events.push((payload.height, payload.state_hash, payload.canonical));
         }
-        *event_counts.entry(event.event_type).or_insert(0) += 1;
     }
 
-    assert_eq!(event_counts.get(&EventType::BlockCanonicityUpdate).cloned().unwrap(), 25);
-
-    // Best Block & Last canonical update:
-    assert_eq!(last_best_block.clone().unwrap().height, 10_u64);
-    assert_eq!(&last_best_block.unwrap().state_hash, "3NKHYHrqKpDcon6ToV5CLDiheanjshk5gcsNqefnK78phCFTR2aL");
+    // Compare the actual and expected events
+    assert_eq!(
+        actual_canonical_events.len(),
+        expected_canonical_events.len(),
+        "Mismatch in the number of events"
+    );
+    assert_eq!(actual_canonical_events, expected_canonical_events, "Events do not match the expected sequence");
 
     Ok(())
 }
