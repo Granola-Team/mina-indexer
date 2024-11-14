4ba40c9891238f5ece2a649893f6f23268c2b6e3
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-11-14 15:30:54 -0400

    decrease the buffer size by 50%

--- rust/src/constants.rs
+++ rust/src/constants.rs
@@ -1,5 +1,5 @@
 pub const GENESIS_STATE_HASH: &str = "3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ";
 pub const TRANSITION_FRONTIER_DISTANCE: usize = 290;
 pub const MAINNET_COINBASE_REWARD: u64 = 720000000000;
-pub const CHANNEL_MESSAGE_CAPACITY: usize = 100_000_000;
+pub const CHANNEL_MESSAGE_CAPACITY: usize = 50_000_000;
 pub const POSTGRES_CONNECTION_STRING: &str = "host=localhost port=9002 user=mina_indexer password=mina_indexer dbname=mina_indexer";
