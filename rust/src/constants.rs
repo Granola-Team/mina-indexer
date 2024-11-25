a4efcc8593faaf3525834a830b9ef23f764f8a60
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-11-25 15:01:48 -0400

    change format of MAINNET_COINBASE_REWARD

--- rust/src/constants.rs
+++ rust/src/constants.rs
@@ -1,6 +1,6 @@
 pub const GENESIS_STATE_HASH: &str = "3NKeMoncuHab5ScarV5ViyF16cJPT4taWNSaTLS64Dp67wuXigPZ";
 pub const TRANSITION_FRONTIER_DISTANCE: usize = 290;
-pub const MAINNET_COINBASE_REWARD: u64 = 720000000000;
+pub const MAINNET_COINBASE_REWARD: u64 = 720_000_000_000;
 pub const CHANNEL_MESSAGE_CAPACITY: usize = 10_000_000;
 pub const POSTGRES_CONNECTION_STRING: &str = "host=localhost port=9002 user=mina_indexer password=mina_indexer dbname=mina_indexer";
 pub const FILE_PUBLISHER_ACTOR_ID: &str = "FilePublisher";
