4cf8cded278b4105c20e2d882c8283640ec1e9f8
Isaac-DeFrain <isaac@granola.team>
2024-12-21 18:46:13 -0800

    fix(tier3): filter on the MINA token ledger

--- ops/indexer-ledger-normalizer.rb
+++ ops/indexer-ledger-normalizer.rb
@@ -6,11 +6,16 @@ require "json"
 
 filename = ARGV[0]
 file_content = File.read(filename)
+
+# All token ledgers
 data = JSON.parse(file_content)
+
+# Get the MINA token ledger
+mina_ledger = data["wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf"]
 
 result = {}
 
-data.each_value do |value|
+mina_ledger.each_value do |value|
   balance_nanomina = value["balance"].to_s.rjust(10, "0")
   balance_mina = "#{balance_nanomina[0..-10]}.#{balance_nanomina[-9..]}"
   normalized_balance = balance_mina.sub(/\.?0+$/, "")
