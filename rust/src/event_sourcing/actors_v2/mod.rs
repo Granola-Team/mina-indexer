9e9d295ddb76dc11bf0dd69d66ab0875a2110774
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-12-30 11:56:51 -0400

    add genesis DAG for ledger and genesis block ingestion

--- rust/src/event_sourcing/actors_v2/mod.rs
+++ rust/src/event_sourcing/actors_v2/mod.rs
@@ -3,6 +3,7 @@ use super::{
     events::Event,
 };
 use account_summary_actor::AccountSummaryActor;
+use account_summary_persistence_actor::AccountSummaryPersistenceActor;
 use accounting_actor::AccountingActor;
 use berkeley_block_actor::BerkeleyBlockActor;
 use block_ancestor_actor::BlockAncestorActor;
@@ -10,6 +11,7 @@ use block_canonicity_actor::BlockCanonicityActor;
 use block_confirmations_actor::BlockConfirmationsActor;
 use canonical_berkeley_block_actor::CanonicalBerkeleyBlockActor;
 use canonical_mainnet_block_actor::CanonicalMainnetBlockActor;
+use identity_actor::IdentityActor;
 use ledger_persistence_actor::LedgerPersistenceActor;
 use mainnet_block_actor::MainnetBlockParserActor;
 use new_account_actor::NewAccountActor;
@@ -19,6 +21,7 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 
 pub(crate) mod account_summary_actor;
+pub(crate) mod account_summary_persistence_actor;
 pub(crate) mod accounting_actor;
 pub(crate) mod berkeley_block_actor;
 pub(crate) mod block_ancestor_actor;
@@ -26,6 +29,7 @@ pub(crate) mod block_canonicity_actor;
 pub(crate) mod block_confirmations_actor;
 pub(crate) mod canonical_berkeley_block_actor;
 pub(crate) mod canonical_mainnet_block_actor;
+pub(crate) mod identity_actor;
 pub(crate) mod ledger_persistence_actor;
 pub(crate) mod mainnet_block_actor;
 pub(crate) mod new_account_actor;
@@ -77,6 +81,9 @@ pub async fn spawn_actor_dag() -> (Arc<Mutex<ActorDAG>>, tokio::sync::mpsc::Send
     let account_summary_node = AccountSummaryActor::create_actor().await;
     let account_summary_node_id = account_summary_node.id();
 
+    let account_summary_pers_node = AccountSummaryPersistenceActor::create_actor(true).await;
+    let account_summary_pers_node_id = account_summary_pers_node.id();
+
     let pcb_sender = dag.set_root(pcb_node);
 
     dag.add_node(mainnet_block_node);
@@ -123,6 +130,9 @@ pub async fn spawn_actor_dag() -> (Arc<Mutex<ActorDAG>>, tokio::sync::mpsc::Send
     dag.add_node(account_summary_node);
     dag.link_parent(&accounting_node_id, &account_summary_node_id);
 
+    dag.add_node(account_summary_pers_node);
+    dag.link_parent(&account_summary_node_id, &account_summary_pers_node_id);
+
     let dag = Arc::new(Mutex::new(dag));
     tokio::spawn({
         let dag = Arc::clone(&dag);
@@ -135,31 +145,64 @@ pub async fn spawn_actor_dag() -> (Arc<Mutex<ActorDAG>>, tokio::sync::mpsc::Send
     (dag, pcb_sender)
 }
 
-pub async fn spawn_genesis_ledger_dag() -> (Arc<Mutex<ActorDAG>>, tokio::sync::mpsc::Sender<Event>) {
+pub async fn spawn_genesis_dag() -> (Arc<Mutex<ActorDAG>>, tokio::sync::mpsc::Sender<Event>) {
     // 1. Create a new DAG.
     let mut dag = ActorDAG::new();
 
+    let identity_node = IdentityActor::create_actor().await;
+    let identity_node_id = identity_node.id();
+
+    // Identity node joins several roots into a single root
+    let sender = dag.set_root(identity_node);
+
+    // Branch 1: For persisting account balances (summary)
+    let account_summary_node = AccountSummaryActor::create_actor().await;
+    let account_summary_node_id = account_summary_node.id();
+    dag.add_node(account_summary_node);
+    dag.link_parent(&identity_node_id, &account_summary_node_id);
+
+    // Leaf
+    let account_summary_pers_node = AccountSummaryPersistenceActor::create_actor(false).await;
+    let account_summary_pers_node_id = account_summary_pers_node.id();
+    dag.add_node(account_summary_pers_node);
+    dag.link_parent(&account_summary_node_id, &account_summary_pers_node_id);
+
+    // Branch 2: For persisting genesis ledger
+    // Leaf
     let ledger_persistence_node = LedgerPersistenceActor::create_actor(false).await;
-    let sender = dag.set_root(ledger_persistence_node);
+    let ledger_persistence_node_id = ledger_persistence_node.id();
+    dag.add_node(ledger_persistence_node);
+    dag.link_parent(&identity_node_id, &ledger_persistence_node_id);
 
-    let dag = Arc::new(Mutex::new(dag));
-    tokio::spawn({
-        let dag = Arc::clone(&dag);
-        async move {
-            dag.lock().await.spawn_all().await;
-        }
-    });
-
-    // 7. Return the root actor’s Sender<Event>.
-    (dag, sender)
-}
-
-pub async fn spawn_preexisting_account_dag() -> (Arc<Mutex<ActorDAG>>, tokio::sync::mpsc::Sender<Event>) {
-    // 1. Create a new DAG.
-    let mut dag = ActorDAG::new();
-
+    // Branch 3: For processing pre-existing accounts
+    // Leaf
     let new_account_node = NewAccountActor::create_actor().await;
-    let sender = dag.set_root(new_account_node);
+    let new_account_node_id = new_account_node.id();
+    dag.add_node(new_account_node);
+    dag.link_parent(&identity_node_id, &new_account_node_id);
+
+    // Branch 4: Persist genesis block ledger
+    let accounting_node = AccountingActor::create_actor().await;
+    let accounting_node_id = accounting_node.id();
+    dag.add_node(accounting_node);
+    dag.link_parent(&identity_node_id, &accounting_node_id);
+
+    // Leaf (for persisting ledger information coming out of genesis block itself)
+    let genesis_block_ledger_persistence_node = LedgerPersistenceActor::create_actor(true).await;
+    let genesis_block_ledger_persistence_node_id = genesis_block_ledger_persistence_node.id();
+    dag.add_node(genesis_block_ledger_persistence_node);
+    dag.link_parent(&accounting_node_id, &genesis_block_ledger_persistence_node_id);
+
+    let account_summary_node = AccountSummaryActor::create_actor().await;
+    let account_summary_node_id = account_summary_node.id();
+    dag.add_node(account_summary_node);
+    dag.link_parent(&accounting_node_id, &account_summary_node_id);
+
+    // Leaf
+    let account_summary_pers_node = AccountSummaryPersistenceActor::create_actor(true).await;
+    let account_summary_pers_node_id = account_summary_pers_node.id();
+    dag.add_node(account_summary_pers_node);
+    dag.link_parent(&account_summary_node_id, &account_summary_pers_node_id);
 
     let dag = Arc::new(Mutex::new(dag));
     tokio::spawn({
