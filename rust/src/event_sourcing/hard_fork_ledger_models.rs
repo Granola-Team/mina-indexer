045d96f6775d695eda0b029a5814d680e9078cf9
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-01-06 09:59:24 -0400

    add method to turn ledger into double entries

--- rust/src/event_sourcing/hard_fork_ledger_models.rs
+++ rust/src/event_sourcing/hard_fork_ledger_models.rs
@@ -1,5 +1,8 @@
+use super::payloads::{AccountingEntry, AccountingEntryAccountType, AccountingEntryType, DoubleEntryRecordPayload, LedgerDestination};
+use crate::constants::MINA_TOKEN_ID;
+use bigdecimal::{BigDecimal, ToPrimitive};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct HardForkLedgerAccount {
@@ -12,6 +15,42 @@ pub struct HardForkLedgerAccount {
 pub struct HardForkLedger {
     #[serde(flatten)]
     pub accounts: HashMap<String, HardForkLedgerAccount>,
+}
+
+impl HardForkLedger {
+    pub fn get_accounting_double_entries(&self) -> Vec<DoubleEntryRecordPayload> {
+        self.accounts
+            .iter()
+            .map(|(pk, account)| {
+                let balance = BigDecimal::from_str(&account.balance).expect("Invalid number format") * BigDecimal::from(1_000_000_000);
+                DoubleEntryRecordPayload {
+                    height: 359604,
+                    state_hash: "n/a".to_string(),
+                    ledger_destination: LedgerDestination::BlockchainLedger,
+                    lhs: vec![AccountingEntry {
+                        counterparty: pk.to_string(),
+                        transfer_type: "HardForkLedger".to_string(),
+                        entry_type: AccountingEntryType::Debit,
+                        account: format!("HardForkLedger#{}", pk.to_string()),
+                        account_type: AccountingEntryAccountType::VirtualAddess,
+                        amount_nanomina: balance.to_u64().expect("Number too large for u64"),
+                        timestamp: 0,
+                        token_id: MINA_TOKEN_ID.to_string(),
+                    }],
+                    rhs: vec![AccountingEntry {
+                        counterparty: format!("HardForkLedger#{}", pk.to_string()),
+                        transfer_type: "HardForkLedger".to_string(),
+                        entry_type: AccountingEntryType::Credit,
+                        account: pk.to_string(),
+                        account_type: AccountingEntryAccountType::BlockchainAddress,
+                        amount_nanomina: balance.to_u64().expect("Number too large for u64"),
+                        timestamp: 0,
+                        token_id: MINA_TOKEN_ID.to_string(),
+                    }],
+                }
+            })
+            .collect::<Vec<DoubleEntryRecordPayload>>()
+    }
 }
 
 #[cfg(test)]
