cdb022193cf08d58db4b67714616470a71880348
Isaac-DeFrain <isaac@granola.team>
2025-01-23 09:46:01 -0800

    refactor(store): improve zkapp action store trait

--- rust/src/store/zkapp/actions.rs
+++ rust/src/store/zkapp/actions.rs
@@ -14,20 +14,34 @@ pub trait ZkappActionStore {
         &self,
         pk: &PublicKey,
         token: &TokenAddress,
-        actions: Vec<ActionState>,
+        actions: &[ActionState],
     ) -> Result<u32>;
 
-    /// Get the `n`th action for the token account
+    /// Get the `index`th action for the token account
     fn get_action(
         &self,
         pk: &PublicKey,
         token: &TokenAddress,
-        n: u32,
+        index: u32,
     ) -> Result<Option<ActionState>>;
+
+    /// Set the `index`th action for the token account
+    fn set_action(
+        &self,
+        pk: &PublicKey,
+        token: &TokenAddress,
+        action: &ActionState,
+        index: u32,
+    ) -> Result<()>;
 
     /// Get the total number of actions associated with the token account
     fn get_num_actions(&self, pk: &PublicKey, token: &TokenAddress) -> Result<Option<u32>>;
 
     /// Remove the specified number of actions from the account
-    fn remove_actions(&self, pk: &PublicKey, token: &TokenAddress, n: u32) -> Result<u32>;
+    ///
+    /// Returns the number of remaining actions
+    fn remove_actions(&self, pk: &PublicKey, token: &TokenAddress, num: u32) -> Result<u32>;
+
+    /// Remove the action at the specified index from the account
+    fn remove_action(&self, pk: &PublicKey, token: &TokenAddress, index: u32) -> Result<()>;
 }
