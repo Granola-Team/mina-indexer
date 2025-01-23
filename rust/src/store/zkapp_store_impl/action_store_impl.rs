53621c8bae2c91dfebf8dd276d40ef8fdb051d17
Isaac-DeFrain <isaac@granola.team>
2025-01-23 09:49:47 -0800

    feat(store): impl zkapp action store trait

--- rust/src/store/zkapp_store_impl/action_store_impl.rs
+++ rust/src/store/zkapp_store_impl/action_store_impl.rs
@@ -1,33 +1,119 @@
 use crate::{
     ledger::{public_key::PublicKey, token::TokenAddress},
     mina_blocks::v2::ActionState,
-    store::{zkapp::actions::ZkappActionStore, IndexerStore, Result},
+    store::{
+        column_families::ColumnFamilyHelpers, zkapp::actions::ZkappActionStore, IndexerStore,
+        Result,
+    },
+    utility::store::{
+        common::from_be_bytes,
+        zkapp::actions::{zkapp_actions_key, zkapp_actions_pk_num_key},
+    },
 };
+use anyhow::Context;
+use log::trace;
 
 impl ZkappActionStore for IndexerStore {
     fn add_actions(
         &self,
-        _pk: &PublicKey,
-        _token: &TokenAddress,
-        _actions: Vec<ActionState>,
+        pk: &PublicKey,
+        token: &TokenAddress,
+        actions: &[ActionState],
     ) -> Result<u32> {
-        todo!()
+        trace!("Adding actions to token account ({pk}, {token}): {actions:?}");
+
+        let idx = self.get_num_actions(pk, token)?.unwrap_or_default();
+        let mut num = idx;
+
+        // add each action
+        for action in actions.iter() {
+            self.set_action(pk, token, action, num)?;
+            num += 1;
+        }
+
+        // update number of actions
+        self.database.put_cf(
+            self.zkapp_actions_pk_num_cf(),
+            zkapp_actions_pk_num_key(token, pk),
+            num.to_be_bytes(),
+        )?;
+
+        Ok(num)
     }
 
     fn get_action(
         &self,
-        _pk: &PublicKey,
-        _token: &TokenAddress,
-        _n: u32,
+        pk: &PublicKey,
+        token: &TokenAddress,
+        index: u32,
     ) -> Result<Option<ActionState>> {
-        todo!()
+        trace!("Getting action {index} for token account ({pk}, {token})");
+
+        Ok(self
+            .database
+            .get_pinned_cf(self.zkapp_actions_cf(), zkapp_actions_key(token, pk, index))?
+            .map(|bytes| {
+                serde_json::from_slice(&bytes)
+                    .context(format!("missing {index} action for ({pk}, {token})"))
+                    .unwrap()
+            }))
     }
 
-    fn get_num_actions(&self, _pk: &PublicKey, _token: &TokenAddress) -> Result<Option<u32>> {
-        todo!()
+    fn set_action(
+        &self,
+        pk: &PublicKey,
+        token: &TokenAddress,
+        action: &ActionState,
+        index: u32,
+    ) -> Result<()> {
+        trace!("Setting action {index} for token account ({pk}, {token})");
+
+        Ok(self.database.put_cf(
+            self.zkapp_actions_cf(),
+            zkapp_actions_key(token, pk, index),
+            serde_json::to_vec(action)?,
+        )?)
     }
 
-    fn remove_actions(&self, _pk: &PublicKey, _token: &TokenAddress, _n: u32) -> Result<u32> {
-        todo!()
+    fn get_num_actions(&self, pk: &PublicKey, token: &TokenAddress) -> Result<Option<u32>> {
+        trace!("Getting number of actions for token account ({pk}, {token})");
+
+        Ok(self
+            .database
+            .get_cf(
+                self.zkapp_actions_pk_num_cf(),
+                zkapp_actions_pk_num_key(token, pk),
+            )?
+            .map(from_be_bytes))
+    }
+
+    fn remove_actions(&self, pk: &PublicKey, token: &TokenAddress, n: u32) -> Result<u32> {
+        trace!("Removing {n} actions from token account ({pk}, {token})");
+
+        let mut num = self.get_num_actions(pk, token)?.unwrap_or_default();
+        assert!(n <= num);
+
+        // remove each action
+        for _ in 0..n {
+            num -= 1;
+            self.remove_action(pk, token, num)?;
+        }
+
+        // update number of actions
+        self.database.put_cf(
+            self.zkapp_actions_pk_num_cf(),
+            zkapp_actions_pk_num_key(token, pk),
+            num.to_be_bytes(),
+        )?;
+
+        Ok(num)
+    }
+
+    fn remove_action(&self, pk: &PublicKey, token: &TokenAddress, index: u32) -> Result<()> {
+        trace!("Removing {index}-th action from token account ({pk}, {token})");
+
+        Ok(self
+            .database
+            .delete_cf(self.zkapp_actions_cf(), zkapp_actions_key(token, pk, index))?)
     }
 }
