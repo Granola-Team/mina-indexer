5a14a9dacd14d08936259ea224c71526027ed2ba
Jonathan Hult <Jonathan@JonathanHult.com>
2025-01-22 22:12:37 -0600

    feat(ops): multi-thread processing for `remove-proofs-from-pcbs.rb`

--- ops/remove-proofs-from-pcbs.rb
+++ ops/remove-proofs-from-pcbs.rb
@@ -4,46 +4,60 @@
 
 require "json"
 require "find"
+require "etc"
 
 # Recursive method to remove all "proofs" properties from a JSON structure
 def remove_proofs(obj)
-  if obj.is_a?(Hash)
+  case obj
+  when Hash
     obj.delete("proofs")
-    obj.each { |key, value| remove_proofs(value) }
-  elsif obj.is_a?(Array)
+    obj.each_value { |value| remove_proofs(value) }
+  when Array
     obj.each { |item| remove_proofs(item) }
   end
 end
 
+# Process a single JSON file
+def process_file(path)
+  json_data = JSON.parse(File.read(path))
+
+    # Remove all occurrences of "proofs"
+  remove_proofs(json_data)
+
+    # Overwrite the original file with compact JSON (single line)
+  File.write(path, JSON.generate(json_data))
+
+  puts "Successfully removed 'proofs' from #{path}"
+rescue Errno::ENOENT
+  puts "File not found: #{path}"
+rescue JSON::ParserError
+  puts "Invalid JSON file: #{path}"
+end
+
 # Process all JSON files in a directory
 def process_directory(directory)
-  Find.find(directory) do |path|
-    if /\.json$/.match?(path)
-      begin
-        file_contents = File.read(path)
-        json_data = JSON.parse(file_contents)
-
-        # Remove all occurrences of "proofs"
-        remove_proofs(json_data)
-
-        # Overwrite the original file with compact JSON (single line)
-        File.write(path, JSON.generate(json_data))
-
-        puts "Successfully removed 'proofs' from #{path}"
-      rescue Errno::ENOENT
-        puts "File not found: #{path}"
-      rescue JSON::ParserError
-        puts "Invalid JSON file: #{path}"
+  thread_queue = Queue.new
+  threads = Array.new(Etc.nprocessors * 4) do
+    Thread.new do
+      while path = thread_queue.pop
+        process_file(path)
       end
     end
   end
+
+  # Feed files to queue as we find them
+  Find.find(directory) do |path|
+    thread_queue << path if path.end_with?('.json')
+  end
+
+  # Signal threads to finish
+  threads.size.times { thread_queue << nil }
+
+  # Wait for all threads to complete
+  threads.each(&:join)
 end
 
 # Usage: ./remove_proofs_from_json_directory_compact.rb <directory>
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <directory>"
-  exit 1
-end
+abort "Usage: #{$PROGRAM_NAME} <directory>" if ARGV.length != 1
 
-directory = ARGV[0]
-process_directory(directory)
+process_directory(ARGV[0])
