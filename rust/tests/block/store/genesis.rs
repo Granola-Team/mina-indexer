8a5841f7598a9180b77c5c2fa7cf44d07f5f326d
Isaac-DeFrain <isaac@granola.team>
2025-01-24 15:39:50 -0800

    test(genesis): add v2 block store unit test

--- rust/tests/block/store/genesis.rs
+++ rust/tests/block/store/genesis.rs
@@ -1,4 +1,4 @@
-use crate::helpers::setup_new_db_dir;
+use crate::helpers::{hardfork_genesis_state, setup_new_db_dir};
 use mina_indexer::{
     block::{genesis::GenesisBlock, store::BlockStore},
     constants::*,
@@ -10,13 +10,13 @@ use mina_indexer::{
 use std::{path::PathBuf, sync::Arc};
 
 #[test]
-fn block_added() -> anyhow::Result<()> {
-    let store_dir = setup_new_db_dir("block-store-genesis")?;
+fn genesis_v1() -> anyhow::Result<()> {
+    let store_dir = setup_new_db_dir("block-store-genesis-v1")?;
     let indexer_store = Arc::new(IndexerStore::new(store_dir.path())?);
     let genesis_ledger_path = &PathBuf::from("./data/genesis_ledgers/mainnet.json");
     let genesis_root = parse_file(genesis_ledger_path)?;
 
-    let indexer = IndexerState::new(
+    let state = IndexerState::new(
         genesis_root.into(),
         IndexerVersion::default(),
         indexer_store,
@@ -26,8 +26,9 @@ fn block_added() -> anyhow::Result<()> {
         false,
     )?;
 
+    // check v1 genesis block is in the block store
     assert_eq!(
-        indexer
+        state
             .indexer_store
             .unwrap()
             .get_block(&MAINNET_GENESIS_HASH.into())
@@ -35,5 +36,61 @@ fn block_added() -> anyhow::Result<()> {
             .map(|b| b.0),
         Some(GenesisBlock::new_v1().unwrap().to_precomputed())
     );
+
+    Ok(())
+}
+
+#[test]
+fn genesis_v2_add() -> anyhow::Result<()> {
+    let store_dir = setup_new_db_dir("block-store-genesis-v2-add")?;
+    let indexer_store = Arc::new(IndexerStore::new(store_dir.path())?);
+    let genesis_ledger_path = &PathBuf::from("./data/genesis_ledgers/mainnet.json");
+    let genesis_root = parse_file(genesis_ledger_path)?;
+
+    let mut state = IndexerState::new(
+        genesis_root.into(),
+        IndexerVersion::default(),
+        indexer_store,
+        MAINNET_CANONICAL_THRESHOLD,
+        MAINNET_TRANSITION_FRONTIER_K,
+        false,
+        false,
+    )?;
+
+    let GenesisBlock(block, block_bytes) = GenesisBlock::new_v2()?;
+
+    // add v2 genesis block
+    assert!(state.block_pipeline(&block, block_bytes)?);
+
+    // check v2 genesis block is in the block store
+    assert_eq!(
+        state
+            .indexer_store
+            .unwrap()
+            .get_block(&HARDFORK_GENESIS_HASH.into())
+            .unwrap()
+            .map(|b| b.0),
+        Some(block)
+    );
+
+    Ok(())
+}
+
+#[test]
+fn genesis_v2_start() -> anyhow::Result<()> {
+    let store_dir = setup_new_db_dir("block-store-genesis-v2-start")?;
+    let state = hardfork_genesis_state(store_dir.path())?;
+
+    // check v2 genesis block is in the block store
+    assert_eq!(
+        state
+            .indexer_store
+            .unwrap()
+            .get_block(&HARDFORK_GENESIS_HASH.into())
+            .unwrap()
+            .map(|b| b.0),
+        Some(GenesisBlock::new_v2().unwrap().to_precomputed())
+    );
+
     Ok(())
 }
