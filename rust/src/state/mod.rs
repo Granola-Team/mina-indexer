b5c5495f3970d3552f14b05f6c778a3588d1b990
Robin Bate Boerop <me@robinbb.com>
2025-01-30 16:36:35 -0800

    Revert "chore: improve log messages"
    This reverts commit 662433693fa6782f7b8dd416d8c59626116be164.

--- rust/src/state/mod.rs
+++ rust/src/state/mod.rs
@@ -542,7 +542,7 @@ impl IndexerState {
         }
 
         self.report_from_block_count(block_parser, total_time);
-        info!("Finished processing canonical chain");
+        info!("Finished processing deep canonical chain");
         info!("Adding recent blocks to the witness tree and orphaned blocks to the block store");
 
         // deep canonical & recent blocks added, now add orphaned blocks
