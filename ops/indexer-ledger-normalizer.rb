f674a21ce4857791b441c85df2b61e1b06b6e6a9
Isaac-DeFrain <isaac@granola.team>
2025-02-10 14:35:53 -0800

    fix(ops): correct mina ledger balance in normalization

--- ops/indexer-ledger-normalizer.rb
+++ ops/indexer-ledger-normalizer.rb
@@ -16,8 +16,7 @@ mina_ledger = data["wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf"]
 result = {}
 
 mina_ledger.each_value do |value|
-  balance_nanomina = value["balance"].to_s.rjust(10, "0")
-  balance_mina = "#{balance_nanomina[0..-10]}.#{balance_nanomina[-9..]}"
+  balance_mina = value["balance"].to_s
   normalized_balance = balance_mina.sub(/\.?0+$/, "")
   nonce = (value["nonce"] || 0).to_s
   delegate = value["delegate"] || value["public_key"]
