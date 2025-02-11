1f7e03d1cdbbec556b2d38d796239ad4f8c8e698
Robin Bate Boerop <me@robinbb.com>
2025-02-10 22:46:30 -0800

    Tighten indexer-ledger-normalizer.rb

--- ops/indexer-ledger-normalizer.rb
+++ ops/indexer-ledger-normalizer.rb
@@ -4,11 +4,8 @@
 
 require "json"
 
-filename = ARGV[0]
-file_content = File.read(filename)
-
 # All token ledgers
-data = JSON.parse(file_content)
+data = JSON.parse(File.read(ARGV[0]))
 
 # Get the MINA token ledger
 mina_ledger = data["wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf"]
@@ -16,16 +13,15 @@ mina_ledger = data["wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf"]
 result = {}
 
 mina_ledger.each_value do |value|
-  balance_mina = value["balance"].to_s
+  pk = value["public_key"]
   nonce = (value["nonce"] || 0).to_s
-  delegate = value["delegate"] || value["public_key"]
-  result[value["public_key"]] = {
+  balance = value["balance"].to_s
+  delegate = value["delegate"] || pk
+  result[pk] = {
     "nonce" => nonce,
-    "balance" => balance_mina,
+    "balance" => balance,
     "delegate" => delegate
   }
 end
 
-sorted_result = result.sort.to_h
-
-puts JSON.pretty_generate(sorted_result)
+puts JSON.pretty_generate(result.sort.to_h)
