d0f83cbaea6985babe6f7e7a9f04022b99c058d8
Isaac-DeFrain <isaac@granola.team>
2025-02-26 11:37:10 -0800

    fix(tier 3): correct ledger normalizer scripts

--- ops/indexer-ledger-normalizer.rb
+++ ops/indexer-ledger-normalizer.rb
@@ -4,24 +4,38 @@
 
 require "json"
 
+MINA_TOKEN = "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf"
+
 # All token ledgers
 data = JSON.parse(File.read(ARGV[0]))
 
-# Get the MINA token ledger
-mina_ledger = data["wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf"]
-
 result = {}
 
-mina_ledger.each_value do |value|
-  pk = value["public_key"]
-  nonce = (value["nonce"] || 0).to_s
-  balance = value["balance"].to_s
-  delegate = value["delegate"] || pk
-  result[pk] = {
-    "nonce" => nonce,
-    "balance" => balance,
-    "delegate" => delegate
-  }
+# Get all token accounts
+data.keys.each do |token|
+  data[token].each_value do |value|
+    pk = value["public_key"]
+    balance = value["balance"].to_s
+    nonce = (value["nonce"] || 0).to_s
+    delegate = value["delegate"] || pk
+
+    result[pk] = if token == MINA_TOKEN
+      # don't add MINA token
+      {
+        "nonce" => nonce,
+        "balance" => balance,
+        "delegate" => delegate
+      }
+    else
+      # add non-MINA token
+      {
+        "nonce" => nonce,
+        "balance" => balance,
+        "delegate" => delegate,
+        "token" => token
+      }
+    end
+  end
 end
 
 puts JSON.pretty_generate(result.sort.to_h)
--- ops/mina-ledger-normalizer.rb
+++ ops/mina-ledger-normalizer.rb
@@ -4,6 +4,8 @@
 
 require "json"
 
+MINA_TOKEN = "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf"
+
 filename = ARGV[0]
 file = File.read(filename)
 accounts = JSON.parse(file)
@@ -14,16 +16,30 @@ accounts = JSON.parse(file)
 
 result = {}
 
+# Normalize all token accounts
 accounts.each do |account|
-  public_key = account["pk"]
-  nonce = account["nonce"] || "0"
+  pk = account["pk"]
   balance = account["balance"]
-  delegate = account["delegate"] || public_key
-  result[public_key] = {
-    "nonce" => nonce,
-    "balance" => balance,
-    "delegate" => delegate
-  }
+  nonce = account["nonce"] || "0"
+  delegate = account["delegate"] || pk
+  token = account["token"] || MINA_TOKEN
+
+  result[pk] = if token == MINA_TOKEN
+    # don't add MINA token
+    {
+      "nonce" => nonce,
+      "balance" => balance,
+      "delegate" => delegate
+    }
+  else
+    # add non-MINA token
+    {
+      "nonce" => nonce,
+      "balance" => balance,
+      "delegate" => delegate,
+      "token" => token
+    }
+  end
 end
 
 sorted_result = result.sort.to_h
