7ded00ec0458643a18267881b6addf1a30aa0a5d
Robin Bate Boerop <me@robinbb.com>
2025-02-26 15:08:30 -0800

    Include some default Linode keys

--- ops/granola-rclone.rb
+++ ops/granola-rclone.rb
@@ -2,8 +2,11 @@
 
 CONFIG_FILE = "#{__dir__}/rclone.conf"
 
-access_key = ENV["LINODE_OBJ_ACCESS_KEY"]
-secret_key = ENV["LINODE_OBJ_SECRET_KEY"]
+DEFAULT_ACCESS_KEY = "HID41DSNO0XVU6XVV12G"
+DEFAULT_SECRET_KEY = "sG1GhHovGsl8ckUzZAdbzMK2xSehlrWH09IVJ6rg"
+
+access_key = ENV["LINODE_OBJ_ACCESS_KEY"] || DEFAULT_ACCESS_KEY
+secret_key = ENV["LINODE_OBJ_SECRET_KEY"] || DEFAULT_SECRET_KEY
 
 args = [
   "rclone",
