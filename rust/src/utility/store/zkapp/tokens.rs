8e3c3d937d3d990f1bfdd13f90750cfc56a95b14
Robin Bate Boerop <me@robinbb.com>
2025-02-27 20:40:43 -0800

    Revert "feat: expand base type functionalities"
    This reverts commit e1e11116f6a1832c2893f4b8c0a249c730154f56.

--- rust/src/utility/store/zkapp/tokens.rs
+++ rust/src/utility/store/zkapp/tokens.rs
@@ -4,7 +4,7 @@ use crate::{
     base::public_key::PublicKey, ledger::token::TokenAddress, utility::store::common::U32_LEN,
 };
 
-/// Use with [zkapp_tokens_holder_cf]
+/// Key to use with [zkapp_tokens_holder_cf]
 pub fn zkapp_tokens_holder_key(
     token: &TokenAddress,
     index: u32,
@@ -17,52 +17,7 @@ pub fn zkapp_tokens_holder_key(
     key
 }
 
-/// Use with [zkapp_tokens_holder_index_cf]
-pub fn zkapp_tokens_holder_index_key(
-    token: &TokenAddress,
-    pk: &PublicKey,
-) -> [u8; TokenAddress::LEN + PublicKey::LEN] {
-    let mut key = [0; TokenAddress::LEN + PublicKey::LEN];
-
-    key[..TokenAddress::LEN].copy_from_slice(token.0.as_bytes());
-    key[TokenAddress::LEN..].copy_from_slice(pk.0.as_bytes());
-
-    key
-}
-
-/// Use with [zkapp_tokens_historical_diffs_cf]
-pub fn zkapp_tokens_historical_diffs_key(
-    token: &TokenAddress,
-    index: u32,
-) -> [u8; TokenAddress::LEN + U32_LEN] {
-    zkapp_tokens_holder_key(token, index)
-}
-
-/// Use with [zkapp_tokens_historical_owners_cf]
-pub fn zkapp_tokens_historical_owners_key(
-    token: &TokenAddress,
-    index: u32,
-) -> [u8; TokenAddress::LEN + U32_LEN] {
-    zkapp_tokens_holder_key(token, index)
-}
-
-/// Use with [zkapp_tokens_historical_symbols_cf]
-pub fn zkapp_tokens_historical_symbols_key(
-    token: &TokenAddress,
-    index: u32,
-) -> [u8; TokenAddress::LEN + U32_LEN] {
-    zkapp_tokens_holder_key(token, index)
-}
-
-/// Use with [zkapp_tokens_historical_supplies_cf]
-pub fn zkapp_tokens_historical_supplies_key(
-    token: &TokenAddress,
-    index: u32,
-) -> [u8; TokenAddress::LEN + U32_LEN] {
-    zkapp_tokens_holder_key(token, index)
-}
-
-/// Use with [zkapp_tokens_pk_cf]
+/// Key to use with [zkapp_tokens_pk_cf]
 pub fn zkapp_tokens_pk_key(pk: &PublicKey, index: u32) -> [u8; PublicKey::LEN + U32_LEN] {
     let mut key = [0; PublicKey::LEN + U32_LEN];
 
@@ -72,20 +27,17 @@ pub fn zkapp_tokens_pk_key(pk: &PublicKey, index: u32) -> [u8; PublicKey::LEN + 
     key
 }
 
-/// Use with [zkapp_tokens_pk_index_cf]
+/// Key to use with [zkapp_tokens_pk_index_cf]
 pub fn zkapp_tokens_pk_index_key(
     token: &TokenAddress,
     pk: &PublicKey,
 ) -> [u8; TokenAddress::LEN + PublicKey::LEN] {
-    zkapp_tokens_holder_index_key(token, pk)
-}
+    let mut key = [0; TokenAddress::LEN + PublicKey::LEN];
 
-/// Use with [zkapp_tokens_historical_pk_diffs_cf]
-pub fn zkapp_tokens_historical_pk_diffs_key(
-    pk: &PublicKey,
-    index: u32,
-) -> [u8; PublicKey::LEN + U32_LEN] {
-    zkapp_tokens_pk_key(pk, index)
+    key[..TokenAddress::LEN].copy_from_slice(token.0.as_bytes());
+    key[TokenAddress::LEN..].copy_from_slice(pk.0.as_bytes());
+
+    key
 }
 
 #[cfg(test)]
@@ -101,6 +53,7 @@ mod tests {
         let index = u32::arbitrary(g);
 
         let key = super::zkapp_tokens_holder_key(&token, index);
+
         assert_eq!(key[..TokenAddress::LEN], *token.0.as_bytes());
         assert_eq!(key[TokenAddress::LEN..], index.to_be_bytes());
 
@@ -115,6 +68,7 @@ mod tests {
         let index = u32::arbitrary(g);
 
         let key = super::zkapp_tokens_pk_key(&pk, index);
+
         assert_eq!(key[..PublicKey::LEN], *pk.0.as_bytes());
         assert_eq!(key[PublicKey::LEN..], index.to_be_bytes());
 
@@ -129,22 +83,9 @@ mod tests {
         let pk = PublicKey::arbitrary(g);
 
         let key = super::zkapp_tokens_pk_index_key(&token, &pk);
+
         assert_eq!(key[..TokenAddress::LEN], *token.0.as_bytes());
         assert_eq!(key[TokenAddress::LEN..], *pk.0.as_bytes());
-
-        Ok(())
-    }
-
-    #[test]
-    fn zkapp_tokens_historical_owners_key() -> anyhow::Result<()> {
-        let g = &mut Gen::new(1000);
-
-        let token = TokenAddress::arbitrary(g);
-        let index = u32::arbitrary(g);
-
-        let key = super::zkapp_tokens_historical_owners_key(&token, index);
-        assert_eq!(key[..TokenAddress::LEN], *token.0.as_bytes());
-        assert_eq!(key[TokenAddress::LEN..], index.to_be_bytes());
 
         Ok(())
     }
