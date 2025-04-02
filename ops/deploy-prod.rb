f034ed4aa509aff2b94af76859af9e5926d32b6b
Robin Bate Boerop <me@robinbb.com>
2025-04-02 07:57:09 -0700

    Correct prod snapshot restore

--- ops/deploy-prod.rb
+++ ops/deploy-prod.rb
@@ -15,18 +15,38 @@ config_base_dir
 config_exe_dir
 config_log_dir
 
-snapshot_name = File.basename(snapshot_path(BLOCKS_COUNT))
+snapshot_name = snapshot_path(BLOCKS_COUNT)
+snapshot_basename = File.basename(snapshot_name)
 if BUILD_TYPE == "nix"
   puts "Fetching snapshot."
   system(
     "#{SRC_TOP}/ops/download-snapshot.sh",
-    snapshot_name,
+    snapshot_basename,
     BASE_DIR
   ) || abort("Failed to download snapshot.")
 else
   puts "Copying snapshot."
-  system("cp #{base_dir("test")}/#{snapshot_name} #{snapshot_path(BLOCKS_COUNT)}") || abort("Failed to move snapshot file.")
+  system("cp #{base_dir("test")}/#{snapshot_basename} #{snapshot_name}") || abort("Failed to move snapshot file.")
 end
+
+my_db_dir = db_dir(BLOCKS_COUNT)
+restore_dir = my_db_dir + ".restore"
+
+# The restore directory must not already exist.
+#
+FileUtils.rm_rf(restore_dir)
+
+puts "Restoring the snapshot to the database directory (#{my_db_dir})."
+
+invoke_mina_indexer(
+  "database", "restore",
+  "--snapshot-file", snapshot_name,
+  "--restore-dir", restore_dir
+) || abort("Snapshot restore failed. Aborting.")
+
+# The snapshot restore succeeded, so we may delete the snapshot.
+#
+FileUtils.rm_rf(snapshot_name)
 
 # Terminate the current version, if any.
 #
@@ -52,20 +72,10 @@ if File.exist? CURRENT
   sleep 1
 end
 
-puts "Restoring the snapshot to the database directory (#{db_dir(BLOCKS_COUNT)})."
-
-# The restore directory must not already exist.
-FileUtils.rm_rf(db_dir(BLOCKS_COUNT))
-
-invoke_mina_indexer(
-  "database", "restore",
-  "--snapshot-file", "#{BASE_DIR}/#{snapshot_name}",
-  "--restore-dir", db_dir(BLOCKS_COUNT)
-) || abort("Snapshot restore failed. Aborting.")
-
-# The snapshot restore succeeded, so we may delete the snapshot.
+# Make the restored snapshot be the database directory.
 #
-FileUtils.rm_rf(snapshot_path(BLOCKS_COUNT))
+FileUtils.rm_rf(my_db_dir)
+FileUtils.mv(restore_dir, my_db_dir)
 
 # Now, we take over.
 #
@@ -87,7 +97,7 @@ else
     " --log-level DEBUG" \
     " --web-hostname 0.0.0.0" \
     " --web-port #{WEB_PORT}" \
-    " --database-dir #{db_dir(BLOCKS_COUNT)}" \
+    " --database-dir #{my_db_dir}" \
     " --staking-ledgers-dir #{LEDGERS_DIR}" \
     " >> #{LOGS_DIR}/out 2>> #{LOGS_DIR}/err"
   puts "Command line: #{command_line}"
