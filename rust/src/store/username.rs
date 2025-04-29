faa469a7e2a12b2427495fee5244fdfb8f7a1086
Isaac-DeFrain <isaac@granola.team>
2025-04-28 17:24:12 -0700

    feat(store): impl username store trait

--- rust/src/store/username.rs
+++ rust/src/store/username.rs
@@ -3,10 +3,11 @@ use crate::{
     base::{public_key::PublicKey, state_hash::StateHash},
     block::store::DbBlockUpdate,
     ledger::username::Username,
+    store::Result,
 };
 use serde::{Deserialize, Serialize};
 use speedb::WriteBatch;
-use std::collections::HashMap;
+use std::collections::{BTreeSet, HashMap};
 
 #[derive(Default, Debug, Serialize, Deserialize)]
 pub struct UsernameUpdate(pub HashMap<PublicKey, Username>);
@@ -14,14 +15,20 @@ pub struct UsernameUpdate(pub HashMap<PublicKey, Username>);
 pub type UsernameAccountUpdate = DbUpdate<UsernameUpdate>;
 
 pub trait UsernameStore {
-    /// Get the username associated with `pk`
-    fn get_username(&self, pk: &PublicKey) -> anyhow::Result<Option<Username>>;
+    /// Get the username associated with pk
+    fn get_username(&self, pk: &PublicKey) -> Result<Option<Username>>;
+
+    /// Add pk's username
+    fn add_username(&self, pk: PublicKey, username: &Username) -> Result<()>;
+
+    /// Remove pk's username
+    fn remove_username(&self, pk: &PublicKey) -> Result<()>;
 
     /// Get number of pk username updates
-    fn get_pk_num_username_updates(&self, pk: &PublicKey) -> anyhow::Result<Option<u32>>;
+    fn get_pk_num_username_updates(&self, pk: &PublicKey) -> Result<Option<u32>>;
 
-    /// Get pk's index-th username
-    fn get_pk_username(&self, pk: &PublicKey, index: u32) -> anyhow::Result<Option<Username>>;
+    /// Get the specified username
+    fn get_pk_username(&self, pk: &PublicKey, index: u32) -> Result<Option<Username>>;
 
     /// Set the user name updates in the block
     fn set_block_username_updates_batch(
@@ -29,17 +36,17 @@ pub trait UsernameStore {
         state_hash: &StateHash,
         username_updates: &UsernameUpdate,
         batch: &mut WriteBatch,
-    ) -> anyhow::Result<()>;
+    ) -> Result<()>;
 
     /// Get the block's username updates
-    fn get_block_username_updates(
-        &self,
-        state_hash: &StateHash,
-    ) -> anyhow::Result<Option<UsernameUpdate>>;
+    fn get_block_username_updates(&self, state_hash: &StateHash) -> Result<Option<UsernameUpdate>>;
+
+    /// Get the accounts associated with the given username
+    fn get_username_pks(&self, username: &str) -> Result<Option<BTreeSet<PublicKey>>>;
 
     /// Update block usernames
-    fn update_block_usernames(&self, blocks: &DbBlockUpdate) -> anyhow::Result<()>;
+    fn update_block_usernames(&self, blocks: &DbBlockUpdate) -> Result<()>;
 
     /// Update usernames
-    fn update_usernames(&self, update: UsernameAccountUpdate) -> anyhow::Result<()>;
+    fn update_usernames(&self, update: UsernameAccountUpdate) -> Result<()>;
 }
--- rust/src/store/username_store_impl.rs
+++ rust/src/store/username_store_impl.rs
@@ -7,20 +7,86 @@ use crate::{
     base::{public_key::PublicKey, state_hash::StateHash},
     block::store::{BlockUpdate, DbBlockUpdate},
     ledger::username::Username,
-    utility::store::common::{from_be_bytes, pk_index_key},
+    store::Result,
+    utility::store::{common::from_be_bytes, username::username_key},
 };
 use log::{error, trace};
 use speedb::WriteBatch;
+use std::collections::BTreeSet;
 
 impl UsernameStore for IndexerStore {
-    fn get_username(&self, pk: &PublicKey) -> anyhow::Result<Option<Username>> {
+    fn get_username(&self, pk: &PublicKey) -> Result<Option<Username>> {
         trace!("Getting {pk} username");
 
-        if let Ok(Some(index)) = self.get_pk_num_username_updates(pk) {
-            return self.get_pk_username(pk, index);
+        if let Ok(Some(num)) = self.get_pk_num_username_updates(pk) {
+            if num == 0 {
+                return Ok(None);
+            }
+
+            return self.get_pk_username(pk, num - 1);
         }
 
         Ok(None)
+    }
+
+    fn add_username(&self, pk: PublicKey, username: &Username) -> Result<()> {
+        trace!("Adding username: {} -> {}", pk, username);
+
+        let index = self.get_pk_num_username_updates(&pk)?.unwrap_or_default();
+        self.database.put_cf(
+            self.username_num_cf(),
+            pk.0.as_bytes(),
+            (index + 1).to_be_bytes(),
+        )?;
+
+        self.database.put_cf(
+            self.username_cf(),
+            username_key(&pk, index),
+            username.0.as_bytes(),
+        )?;
+
+        // update username pks
+        let mut pks = self.get_username_pks(&username.0)?.unwrap_or_default();
+        pks.insert(pk);
+
+        self.database.put_cf(
+            self.username_pk_cf(),
+            username.0.as_bytes(),
+            serde_json::to_vec(&pks)?,
+        )?;
+
+        Ok(())
+    }
+
+    fn remove_username(&self, pk: &PublicKey) -> Result<()> {
+        trace!("Removing username {}", pk);
+
+        if let Some(num) = self.get_pk_num_username_updates(pk)? {
+            assert!(num > 0, "No username to unapply {}", pk);
+
+            // update account usernames
+            let index = num - 1;
+            let username = self.get_username(pk)?.expect("remove username");
+
+            self.database
+                .delete_cf(self.username_cf(), username_key(pk, index))?;
+            self.database
+                .put_cf(self.username_num_cf(), pk.0.as_bytes(), index.to_be_bytes())?;
+
+            // update username accounts
+            let mut pks = self.get_username_pks(&username.0)?.unwrap_or_default();
+            pks.remove(pk);
+
+            self.database.put_cf(
+                self.username_pk_cf(),
+                username.0.as_bytes(),
+                serde_json::to_vec(&pks)?,
+            )?;
+        } else {
+            error!("Invalid username num {}", pk);
+        }
+
+        Ok(())
     }
 
     fn set_block_username_updates_batch(
@@ -28,28 +94,29 @@ impl UsernameStore for IndexerStore {
         state_hash: &StateHash,
         username_updates: &UsernameUpdate,
         batch: &mut WriteBatch,
-    ) -> anyhow::Result<()> {
+    ) -> Result<()> {
         trace!("Setting block username updates {state_hash}");
+
         batch.put_cf(
             self.usernames_per_block_cf(),
             state_hash.0.as_bytes(),
             serde_json::to_vec(username_updates)?,
         );
+
         Ok(())
     }
 
-    fn get_block_username_updates(
-        &self,
-        state_hash: &StateHash,
-    ) -> anyhow::Result<Option<UsernameUpdate>> {
+    fn get_block_username_updates(&self, state_hash: &StateHash) -> Result<Option<UsernameUpdate>> {
         trace!("Getting block username updates {state_hash}");
         Ok(self
             .database
             .get_cf(self.usernames_per_block_cf(), state_hash.0.as_bytes())?
-            .map(|bytes| serde_json::from_slice(&bytes).expect("msg")))
+            .map(|bytes| serde_json::from_slice(&bytes).expect("block username updates")))
     }
 
-    fn update_block_usernames(&self, blocks: &DbBlockUpdate) -> anyhow::Result<()> {
+    fn update_block_usernames(&self, blocks: &DbBlockUpdate) -> Result<()> {
+        trace!("Updating block usernames {:?}", blocks);
+
         let username_updates = DbUpdate {
             apply: blocks
                 .apply
@@ -57,7 +124,7 @@ impl UsernameStore for IndexerStore {
                 .map(|BlockUpdate { state_hash: a, .. }| {
                     self.get_block_username_updates(a)
                         .unwrap()
-                        .expect("username update")
+                        .expect("username update apply")
                 })
                 .collect(),
             unapply: blocks
@@ -66,7 +133,7 @@ impl UsernameStore for IndexerStore {
                 .map(|BlockUpdate { state_hash: u, .. }| {
                     self.get_block_username_updates(u)
                         .unwrap()
-                        .expect("username update")
+                        .expect("username update unapply")
                 })
                 .collect(),
         };
@@ -74,75 +141,50 @@ impl UsernameStore for IndexerStore {
         self.update_usernames(username_updates)
     }
 
-    fn update_usernames(&self, update: UsernameAccountUpdate) -> anyhow::Result<()> {
-        trace!("Updating usernames");
+    fn update_usernames(&self, update: UsernameAccountUpdate) -> Result<()> {
+        trace!("Updating usernames {:?}", update);
 
         // unapply
         for updates in update.unapply {
             for pk in updates.0.keys() {
-                if let Some(num) = self.get_pk_num_username_updates(pk)? {
-                    // decr pk num username updates
-                    if num == 0 {
-                        // remove pk
-                        self.database
-                            .delete_cf(self.username_pk_num_cf(), pk.0.as_bytes())?;
-                    } else {
-                        // decrement username update num
-                        self.database.put_cf(
-                            self.username_pk_num_cf(),
-                            pk.0.as_bytes(),
-                            (num - 1).to_be_bytes(),
-                        )?;
-                    }
-
-                    // drop last username update
-                    self.database
-                        .delete_cf(self.username_pk_index_cf(), pk_index_key(pk, num))?;
-                } else {
-                    error!("Invalid username pk num {pk}");
-                }
+                self.remove_username(pk)?;
             }
         }
 
         // apply
         for updates in update.apply {
             for (pk, username) in updates.0 {
-                let index = self
-                    .get_pk_num_username_updates(&pk)?
-                    .map_or(0, |num| num + 1);
-
-                // update num
-                self.database.put_cf(
-                    self.username_pk_num_cf(),
-                    pk.0.as_bytes(),
-                    index.to_be_bytes(),
-                )?;
-
-                // set current username
-                self.database.put_cf(
-                    self.username_pk_index_cf(),
-                    pk_index_key(&pk, index),
-                    username.0.as_bytes(),
-                )?;
+                self.add_username(pk, &username)?;
             }
         }
 
         Ok(())
     }
 
-    fn get_pk_username(&self, pk: &PublicKey, index: u32) -> anyhow::Result<Option<Username>> {
-        trace!("Getting pk's {index}th username {pk}");
+    fn get_pk_username(&self, pk: &PublicKey, index: u32) -> Result<Option<Username>> {
+        trace!("Getting username {} index {}", pk, index);
+
         Ok(self
             .database
-            .get_cf(self.username_pk_index_cf(), pk_index_key(pk, index))?
+            .get_cf(self.username_cf(), username_key(pk, index))?
             .map(|bytes| Username::from_bytes(bytes).expect("username")))
     }
 
-    fn get_pk_num_username_updates(&self, pk: &PublicKey) -> anyhow::Result<Option<u32>> {
-        trace!("Getting pk's number of username updates {pk}");
+    fn get_pk_num_username_updates(&self, pk: &PublicKey) -> Result<Option<u32>> {
+        trace!("Getting username update count {}", pk);
+
         Ok(self
             .database
-            .get_cf(self.username_pk_num_cf(), pk.0.as_bytes())?
+            .get_cf(self.username_num_cf(), pk.0.as_bytes())?
             .map(from_be_bytes))
+    }
+
+    fn get_username_pks(&self, username: &str) -> Result<Option<BTreeSet<PublicKey>>> {
+        trace!("Getting username {} accounts", username);
+
+        Ok(self
+            .database
+            .get_cf(self.username_pk_cf(), username.as_bytes())?
+            .and_then(|bytes| serde_json::from_slice(&bytes).expect("username pks")))
     }
 }
