c91031ab1c263ca06207b7b887d670d0eecb5009
Isaac-DeFrain <isaac@granola.team>
2025-05-08 17:39:10 -0700

    feat(gql): add various pk structs for backwards compatibility

--- rust/src/web/graphql/pk.rs
+++ rust/src/web/graphql/pk.rs
@@ -1,4 +1,4 @@
-//! GQL pk
+//! GQL public key/username pairs
 
 use crate::{
     base::public_key::PublicKey,
@@ -14,6 +14,53 @@ pub struct PK {
     pub username: String,
 }
 
+#[derive(Default, Clone, Debug, PartialEq, SimpleObject, Serialize)]
+pub struct PK_ {
+    #[graphql(name = "public_key")]
+    pub public_key: String,
+    pub username: String,
+}
+
+#[derive(Default, Clone, Debug, PartialEq, SimpleObject, Serialize)]
+pub struct DelegatePK {
+    pub delegate: String,
+    pub delegate_username: String,
+}
+
+#[derive(Default, Clone, Debug, PartialEq, SimpleObject, Serialize)]
+pub struct CreatorPK {
+    pub creator: String,
+    pub creator_username: String,
+}
+
+#[derive(Default, Clone, Debug, PartialEq, SimpleObject, Serialize)]
+pub struct CoinbaseReceiverPK {
+    pub coinbase_receiver: String,
+    pub coinbase_receiver_username: String,
+}
+
+#[derive(Default, Clone, Debug, PartialEq, SimpleObject, Serialize)]
+pub struct ProverPK {
+    pub prover: String,
+    pub prover_username: String,
+}
+
+#[derive(Default, Clone, Debug, PartialEq, SimpleObject, Serialize)]
+pub struct RecipientPK {
+    pub recipient: String,
+    pub recipient_username: String,
+}
+
+#[derive(Default, Clone, Debug, PartialEq, SimpleObject, Serialize)]
+pub struct SenderPK {
+    pub sender: String,
+    pub sender_username: String,
+}
+
+///////////
+// impls //
+///////////
+
 impl PK {
     pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
         Self {
@@ -23,6 +70,115 @@ impl PK {
                 .unwrap_or_default()
                 .0,
             public_key: pk.0,
+        }
+    }
+}
+
+impl PK_ {
+    pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
+        PK::new(db, pk).into()
+    }
+}
+
+impl DelegatePK {
+    pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
+        PK::new(db, pk).into()
+    }
+}
+
+impl CreatorPK {
+    pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
+        PK::new(db, pk).into()
+    }
+}
+
+impl CoinbaseReceiverPK {
+    pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
+        PK::new(db, pk).into()
+    }
+}
+
+impl ProverPK {
+    pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
+        PK::new(db, pk).into()
+    }
+}
+
+impl RecipientPK {
+    pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
+        PK::new(db, pk).into()
+    }
+}
+
+impl SenderPK {
+    pub fn new(db: &std::sync::Arc<IndexerStore>, pk: PublicKey) -> Self {
+        PK::new(db, pk).into()
+    }
+}
+
+/////////////////
+// conversions //
+/////////////////
+
+impl From<PK> for PK_ {
+    fn from(value: PK) -> Self {
+        Self {
+            public_key: value.public_key,
+            username: value.username,
+        }
+    }
+}
+
+impl From<PK> for DelegatePK {
+    fn from(value: PK) -> Self {
+        Self {
+            delegate: value.public_key,
+            delegate_username: value.username,
+        }
+    }
+}
+
+impl From<PK> for CoinbaseReceiverPK {
+    fn from(value: PK) -> Self {
+        Self {
+            coinbase_receiver: value.public_key,
+            coinbase_receiver_username: value.username,
+        }
+    }
+}
+
+impl From<PK> for CreatorPK {
+    fn from(value: PK) -> Self {
+        Self {
+            creator: value.public_key,
+            creator_username: value.username,
+        }
+    }
+}
+
+impl From<PK> for ProverPK {
+    fn from(value: PK) -> Self {
+        Self {
+            prover: value.public_key,
+            prover_username: value.username,
+        }
+    }
+}
+
+impl From<PK> for RecipientPK {
+    fn from(value: PK) -> Self {
+        Self {
+            recipient: value.public_key,
+            recipient_username: value.username,
+        }
+    }
+}
+
+impl From<PK> for SenderPK {
+    fn from(value: PK) -> Self {
+        Self {
+            sender: value.public_key,
+            sender_username: value.username,
         }
     }
 }
